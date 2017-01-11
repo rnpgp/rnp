1aa9774b0f49aa2a5f25f96d8c0452dd2adceef9
Jeffrey Lau <jeffrey.lau@ribose.com>
2017-01-11 11:43:40 +0800

    Make package-builder.sh more flexible in handling paths

--- packaging/redhat/extra/package-builder.sh
+++ packaging/redhat/extra/package-builder.sh
@@ -1,19 +1,59 @@
-# This is a project-specific variant of a script written by Frank Trampe.
+#!/bin/bash
+#
+# Jan 2017
+#
+# This is a project-specific variant of a script written by Frank Trampe,
+# with modifications by Jeffrey Lau.
+#
 # It is hereby released under the license of the enclosing project.
-# Call this with the desired version number as the first argument.
-PNAME=netpgp;
-PVERSION="$1";
-# Make a new copy of the sources and name by version, clearing any remnant from before.
-if [ -e "$PNAME"-"$PVERSION" ]; then rm -rf "$PNAME"-"$PVERSION"; fi;
-cp -pRP "$PNAME" "$PNAME"-"$PVERSION";
-# Clean the new sources.
-(cd "$PNAME"-"$PVERSION"; if [ -e .git ]; then git clean -fdx; git reset --hard; fi;)
-# Make the source tarball for the build.
-tar -cjf "$PNAME"-"$PVERSION".tar.bz2 "$PNAME"-"$PVERSION";
-# Copy the source tarball into the source directory for rpmbuild.
-cp -pRP "$PNAME"-"$PVERSION".tar.bz2 ~/rpmbuild/SOURCES/;
-# Generate the spec file.
-m4 -D "PACKAGE_VERSION=""$PVERSION" -D "PREFIX=/usr" -D "SOURCE_TARBALL_NAME="$PNAME"-""$PVERSION"".tar.bz2" < "$PNAME"-"$PVERSION"/packaging/redhat/m4/rpm.spec > "$PNAME"-""$PVERSION"".spec ;
-# Build the packages.
-rpmbuild -ba --nodeps "$PNAME"-""$PVERSION"".spec;
+# Call this with:
+#  - the desired version number as the first argument;
+#  - the path where the source code is as the second optional argument;
+#    (default: the same as the package name, $PNAME.
+#  - the directory to place the generated spec file as the third optional
+#    argument.
+#    (default: ~/rpmbuild/SPECS)
 
+usage() {
+	echo "Usage: ${0} <version> [source_path] [target_spec_dir]" >&2
+}
+
+main() {
+	# Make sure at least the version is supplied.
+	if [ $# -lt 1 ]; then usage ; exit 1; fi
+	local PNAME=netpgp
+	local PVERSION="$1"
+	local PPATH="${2:-${PNAME}}"
+	local SPEC_DIR="${3:-${HOME}/rpmbuild/SPECS}"
+	local PNAMEVERSION="${PNAME}-${PVERSION}"
+	local SOURCES_DIR="${SOURCES_DIR:-${HOME}/rpmbuild/SOURCES}"
+
+	# Make a new copy of the sources and name by version, clearing any remnant from before.
+	if [ -e "${PNAMEVERSION}" ]; then rm -rf "${PNAMEVERSION}"; fi
+	cp -pRP "$PPATH" "${PNAMEVERSION}"
+
+	# Clean the new sources.
+	# Make sure to commit everything first before running this script!
+	# (cd "${PNAMEVERSION}"; if [ -e .git ]; then git clean -fdx; git reset --hard; fi;)
+
+	# Make the source tarball for the build.
+	tar -cjf "${PNAMEVERSION}".tar.bz2 "${PNAMEVERSION}"
+
+	# Copy the source tarball into the source directory for rpmbuild.
+	local PSOURCE_PATH="${SOURCES_DIR}/"${PNAMEVERSION}".tar.bz2"
+	cp -pRP "${PNAMEVERSION}".tar.bz2 "${PSOURCE_PATH}"
+	chown $(id -u):$(id -g) "${PSOURCE_PATH}"
+
+	# Generate the spec file.
+	local PSPEC_PATH="${SPEC_DIR}/${PNAMEVERSION}.spec"
+	chown $(id -u):$(id -g) "${PSPEC_PATH}"
+	m4 \
+		-D "PACKAGE_VERSION=${PVERSION}" \
+		-D "PREFIX=/usr" \
+		-D "SOURCE_TARBALL_NAME=${PSOURCE_PATH}" < "${PNAMEVERSION}"/packaging/redhat/m4/rpm.spec > "${PSPEC_PATH}"
+
+	# Build the packages.
+	rpmbuild -ba --nodeps "${PSPEC_PATH}"
+}
+
+main "$@"
