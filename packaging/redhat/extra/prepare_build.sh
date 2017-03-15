a34caa09de0f5caee48499fcc5527c38ce7c39e0
Ronald Tse <ronald.tse@ribose.com>
2017-03-15 11:47:47 +0800

    Add git dependency

--- packaging/redhat/extra/prepare_build.sh
+++ packaging/redhat/extra/prepare_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum install -y automake gcc make openssl-devel zlib-devel bzip2-devel libtool \
-  rpmdevtools rpm-build rpm-sign chrpath createrepo rpmlint
+  rpmdevtools rpm-build rpm-sign chrpath createrepo rpmlint git
 
 # yum install -y fedora-review
 
