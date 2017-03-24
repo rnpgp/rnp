5730aa83ccde3467dd1f32bb1e30706b3df35784
Shaun Wheelhouse <shaun@sigio.is>
2017-03-24 15:59:10 +1100

    Temporarily patched the segfaulting bug in lib/netpgp.c:get_last_ring, refactored some of that code, added some documentation to the dynamic array macro.

--- src/lib/netpgp.c
+++ src/lib/netpgp.c
@@ -54,6 +54,7 @@ __RCSID("$NetBSD: netpgp.c,v 1.98 2016/06/28 16:34:40 christos Exp $");
 #include <regex.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <time.h>
 
@@ -330,23 +331,69 @@ readsshkeys(netpgp_t *netpgp, char *homedir, const char *needseckey)
 	return 1;
 }
 
-/* get the uid of the first key in the keyring */
+/* Format a PGP key to a readable hexadecimal string in a user supplied
+ * buffer.
+ *
+ * buffer: the buffer to write into
+ * sigid:  the PGP key ID to format
+ * len:    the length of buffer, including the null terminator
+ *
+ * TODO: There is no error checking here.
+ * TODO: Make this function more general or use an existing one.
+ */
+
+static void
+format_key(char *buffer, uint8_t *sigid, int len)
+{
+	unsigned int i;
+	unsigned int n;
+
+	/* Chunks of two bytes are processed at a time because we can
+	 * always be reasonably sure that PGP_KEY_ID_SIZE will be
+	 * divisible by two. However, if the RFCs specify a fixed
+	 * fixed size for PGP key IDs it might be more constructive
+	 * to format this in one call and do a compile-time size
+	 * check of the constant. If somebody wanted to do
+	 * something exotic they can easily re-implement
+	 * this function.
+	 */
+	for (i = 0, n = 0; i < PGP_KEY_ID_SIZE; i += 2) {
+		n += snprintf(&buffer[n], len - n, "%02x%02x",
+				sigid[i], sigid[i + 1]);
+	}
+	buffer[n] = 0x0;
+}
+
+/* Get the uid of the first key in the keyring.
+ *
+ * TODO: Set errno on failure.
+ * TODO: Check upstream calls to this function - they likely won't
+ *       handle the new error condition.
+ */
 static int
 get_first_ring(pgp_keyring_t *ring, char *id, size_t len, int last)
 {
 	uint8_t	*src;
-	int	 i;
-	int	 n;
 
-	if (ring == NULL) {
+	/* The NULL test on the ring may not be necessary for non-debug
+	 * builds - it would be much better that a NULL ring never
+	 * arrived here in the first place.
+	 *
+	 * The ring length check is a temporary fix for a case where
+	 * an empty ring arrives and causes an access violation in
+	 * some circumstances.
+	 */
+
+	errno = 0;
+
+	if (ring == NULL || ring->keyc < 1) {
+		errno = EINVAL;
 		return 0;
 	}
-	(void) memset(id, 0x0, len);
-	src = ring->keys[(last) ? ring->keyc - 1 : 0].sigid;
-	for (i = 0, n = 0 ; i < PGP_KEY_ID_SIZE ; i += 2) {
-		n += snprintf(&id[n], len - n, "%02x%02x", src[i], src[i + 1]);
-	}
-	id[n] = 0x0;
+
+	memset(id, 0x0, len);
+	src = (uint8_t *) &ring->keys[(last) ? ring->keyc - 1 : 0].sigid;
+	format_key(id, src, len);
 	return 1;
 }
 
