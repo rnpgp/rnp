e0e26f93b4d13c2482380b33b707c47ae9304902
Ronald Tse <ronald.tse@ribose.com>
2017-04-25 13:07:17 +0800

    Allow RPM build process to use PACKAGE_VERSION

--- packaging/redhat/extra/build_rpm.sh
+++ packaging/redhat/extra/build_rpm.sh
@@ -2,18 +2,18 @@
 #
 # (c) 2017 Ribose Inc.
 #
-# Specify the version (default: 1) of the RPM with the first argument or 
+# Specify the version (default: 1) of the RPM with the first argument or
 # $VERSION env var.
 
-# Try to read the NETPGP_BASE_VERSION from source and take it as the RPM 
-# version number.
+# Try to read PACKAGE_VERSION from source and take it as the RPM version
+# number.
 
 readonly __file_dir=$(dirname $0)
-readonly version_file=${__file_dir}/../../../src/lib/version.h
+readonly version_file=${__file_dir}/../../../src/lib/config.h
 
 if [ -r ${version_file} ]; then
   readonly default_version=$( \
-    /usr/bin/env grep define.*RNP_BASE_VERSION ${version_file} | \
+    /usr/bin/env grep define.*PACKAGE_VERSION ${version_file} | \
     awk '{print $3}' | \
     sed 's/"//g' \
   )
