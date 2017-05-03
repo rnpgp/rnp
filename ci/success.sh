486051462ee54ca1eb052950e50f7525e5a55a98
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-05-03 11:42:50 -0400

    CI: Use GCC for test coverage. The version of clang and gcov in the container are not compatible. The normal and sanitize builds now use clang, and the coverage build uses GCC.

--- ci/success.sh
+++ ci/success.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -eu
 
+if [ "$BUILD_MODE" = "coverage" ]; then
+  cd src/cmocka
+  gcov-4.8 --object-file rnp_tests-rnp_tests.o rnp_tests.c
+  bash <(curl -s https://codecov.io/bash)
+fi
+
