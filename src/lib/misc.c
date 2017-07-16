0ff803f4f0f171d8ec5f964a2916a8131de72cf7
Krzysztof Kwiatkowski <contact@amongbytes.com>
2017-07-16 23:04:18 +0100

    Set hash algorithm while calculating fingerprint

--- src/lib/misc.c
+++ src/lib/misc.c
@@ -397,6 +397,7 @@ pgp_fingerprint(pgp_fingerprint_t *fp, const pgp_pubkey_t *key)
         if (rnp_get_debug(__FILE__)) {
             hexdump(stderr, "v2/v3 fingerprint", fp->fingerprint, fp->length);
         }
+        fp->hashtype = PGP_HASH_MD5;
     } else if (key->version == 4) {
         mem = pgp_memory_new();
         if (mem == NULL) {
@@ -418,6 +419,7 @@ pgp_fingerprint(pgp_fingerprint_t *fp, const pgp_pubkey_t *key)
         if (rnp_get_debug(__FILE__)) {
             hexdump(stderr, "sha1 fingerprint", fp->fingerprint, fp->length);
         }
+        fp->hashtype = PGP_HASH_SHA1;
     } else {
         (void) fprintf(stderr, "pgp_fingerprint: unsupported key version\n");
         return RNP_FAIL;
