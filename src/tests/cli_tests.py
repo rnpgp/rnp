ef5b7c3916cfc9fdb9b3a27b4fab549c1981f264
Nickolay Olshevsky <o.nickolay@gmail.com>
2017-07-21 15:41:49 +0300

    Finished first 'full' test with checks for RSA key generation

--- src/tests/cli_tests.py
+++ src/tests/cli_tests.py
@@ -3,12 +3,38 @@
 import sys
 import distutils.spawn
 import tempfile
-from os import path
 import os
+from os import path
 import shutil
 import subprocess
+import re
 from subprocess import Popen, PIPE
 from cli_common import find_utility, run_proc, pswd_pipe
+
+RNP = ''
+RNPK = ''
+GPG = ''
+RNPDIR = ''
+PASSWORD = 'password'
+#RE_RSA_KEY = r'(?ms)# off=0 ctb=c6 tag=6 hlen=3 plen=\d+ new-ctb\s+.*'
+
+RE_RSA_KEY = r'^\s*' \
+r'# off=0 ctb=c6 tag=6 hlen=3 plen=\d+ new-ctb\s+' \
+r':public key packet:\s+' \
+r'version 4, algo 1, created \d+, expires 0\s+' \
+r'pkey\[0\]: \[204[0-8] bits\]\s+' \
+r'pkey\[1\]: \[17 bits\]\s+' \
+r'keyid: ([0-9A-F]{16})\s+' \
+r'# off=\d+ ctb=cd tag=13 hlen=\d+ plen=\d+ new-ctb\s+' \
+r':user ID packet: "rsa@rnp"\s+' \
+r'# off=\d+ ctb=c2 tag=2 hlen=3 plen=\d+ new-ctb\s+' \
+r':signature packet: algo 1, keyid \1\s+' \
+r'version 4, created \d+, md5len 0, sigclass 0x13\s+' \
+r'digest algo 8, begin of digest [0-9a-f\s]{5}\s+' \
+r'hashed subpkt 2 len 4 \(sig created .+\)\s+' \
+r'hashed subpkt 16 len 8 \(issuer key ID \1\)\s+' \
+r'hashed subpkt 25 len 1 \(primary user ID\)\s+' \
+r'data: \[204[0-8] bits\]$'
 
 def setup():
     return
@@ -17,18 +43,42 @@ def run_rnp_tests():
     
     return
 
-def run_rnpkeys_tests():
-    RNPDIR = path.join(os.getcwd(), '.rnp')
-    RNPK = find_utility('rnpkeys')
-    os.mkdir(RNPDIR, 0700)
+def check_packets(fname, regexp):
+    global GPG
 
-    retcode, output, err = run_proc(RNPK, ['--homedir', RNPDIR, '--pass-fd', str(pswd_pipe('password')), '--userid', 'rsa@rnp', '--generate-key'])
+    retcode, output, err = run_proc(GPG, ['--list-packets', fname])
+    if retcode != 0:
+        print err
+        return False
+    else:
+        result = re.match(regexp, output)
+        if result:
+            return True
+        else:
+            print 'Wrong packets: \n' + output
+            return False
+
+def rnpkey_generate_rsa():
+    retcode, output, err = run_proc(RNPK, ['--homedir', RNPDIR, '--pass-fd', str(pswd_pipe(PASSWORD)), '--userid', 'rsa@rnp', '--generate-key'])
 
     if retcode != 0:
         print err
-        raise NameError('rnpkeys failed')
-    else:
-        print output
+        raise NameError('key generation failed')
+    if not check_packets(path.join(RNPDIR, 'pubring.gpg'), RE_RSA_KEY):
+        raise NameError('generated key check failed')
+
+    return
+
+def run_rnpkeys_tests():
+    # Setting up directories
+    global RNPDIR, RNPK, GPG
+    RNPDIR = path.join(os.getcwd(), '.rnp')
+    RNPK = find_utility('rnpkeys')
+    GPG = find_utility('gpg')
+    os.mkdir(RNPDIR, 0700)
+
+    # 1. Generate default RSA key
+    rnpkey_generate_rsa()
 
     return
 
