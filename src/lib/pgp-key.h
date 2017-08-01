a1dc82bd966bd63515f9d91e8d845085031091fb
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-08-01 15:33:20 -0400

    Add separate function for freeing user preferences.

--- src/lib/pgp-key.h
+++ src/lib/pgp-key.h
@@ -72,6 +72,8 @@ void pgp_key_free(pgp_key_t *);
  **/
 void pgp_key_free_data(pgp_key_t *);
 
+void pgp_free_user_prefs(pgp_user_prefs_t *prefs);
+
 const pgp_pubkey_t *pgp_get_pubkey(const pgp_key_t *);
 
 bool pgp_is_key_public(const pgp_key_t *);
