f7b7eb5dda7d65868393d4b4186fe3901ffb2d4c
Krzysztof Kwiatkowski <contact@amongbytes.com>
2017-08-10 00:31:11 +0100

    ECDSA Reduce message size to base point order before signing

--- src/lib/ecdsa.c
+++ src/lib/ecdsa.c
@@ -199,19 +199,23 @@ pgp_ecdsa_sign_hash(pgp_ecc_sig_t *         sign,
                     const pgp_ecc_seckey_t *seckey,
                     const pgp_ecc_pubkey_t *pubkey)
 {
-    botan_pk_op_sign_t signer = NULL;
-    botan_privkey_t    key = NULL;
-    botan_rng_t        rng = NULL;
-    pgp_errcode_t      ret = PGP_E_FAIL;
-    uint8_t            out_buf[2 * MAX_CURVE_BYTELEN] = {0};
-    const size_t       sign_half_len = BITS_TO_BYTES(ec_curves[pubkey->curve].bitlen);
+    botan_pk_op_sign_t     signer = NULL;
+    botan_privkey_t        key = NULL;
+    botan_rng_t            rng = NULL;
+    pgp_errcode_t          ret = PGP_E_FAIL;
+    uint8_t                out_buf[2 * MAX_CURVE_BYTELEN] = {0};
+    const ec_curve_desc_t *curve = get_curve_desc(pubkey->curve);
+
+    if (!curve) {
+        return PGP_E_FAIL;
+    }
 
     if (sign->r || sign->s) {
         // Caller must not allocate r and s
         return PGP_E_FAIL;
     }
 
-    if (botan_privkey_load_ecdsa(&key, seckey->x->mp, ec_curves[pubkey->curve].botan_name)) {
+    if (botan_privkey_load_ecdsa(&key, seckey->x->mp, curve->botan_name)) {
         RNP_LOG("Can't load private key");
         return PGP_E_FAIL;
     }
@@ -224,19 +228,21 @@ pgp_ecdsa_sign_hash(pgp_ecc_sig_t *         sign,
         goto end;
     }
 
-    if (botan_pk_op_sign_update(signer, hashbuf, hash_len)) {
+    const size_t curve_order = BITS_TO_BYTES(curve->bitlen);
+    const size_t leftmost_bytes = hash_len > curve_order ? curve_order : hash_len;
+    if (botan_pk_op_sign_update(signer, &hashbuf[hash_len - leftmost_bytes], leftmost_bytes)) {
         goto end;
     }
 
-    size_t sig_len = 2 * sign_half_len;
+    size_t sig_len = 2 * curve_order;
     if (botan_pk_op_sign_finish(signer, rng, out_buf, &sig_len)) {
         RNP_LOG("Signing failed");
         goto end;
     }
 
     // Allocate memory and copy results
-    sign->r = BN_bin2bn(out_buf, sign_half_len, sign->r);
-    sign->s = BN_bin2bn(out_buf + sign_half_len, sign_half_len, sign->s);
+    sign->r = BN_bin2bn(out_buf, curve_order, sign->r);
+    sign->s = BN_bin2bn(out_buf + curve_order, curve_order, sign->s);
     if (!sign->r || !sign->s) {
         goto end;
     }
@@ -268,8 +274,13 @@ pgp_ecdsa_verify_hash(const pgp_ecc_sig_t *   sign,
     botan_pk_op_verify_t verifier = NULL;
     pgp_errcode_t        ret = PGP_E_V_BAD_SIGNATURE;
     uint8_t              sign_buf[2 * MAX_CURVE_BYTELEN] = {0};
-    const size_t         sign_half_len = BITS_TO_BYTES(ec_curves[pubkey->curve].bitlen);
     uint8_t              point_bytes[BITS_TO_BYTES(521) * 2 + 1] = {0};
+
+    const ec_curve_desc_t *curve = get_curve_desc(pubkey->curve);
+
+    if (!curve) {
+        return PGP_E_FAIL;
+    }
 
     if ((BN_num_bytes(pubkey->point) > sizeof(point_bytes)) ||
         BN_bn2bin(pubkey->point, point_bytes) || (point_bytes[0] != 0x04)) {
@@ -277,14 +288,14 @@ pgp_ecdsa_verify_hash(const pgp_ecc_sig_t *   sign,
         goto end;
     }
 
+    const size_t curve_order = BITS_TO_BYTES(curve->bitlen);
     if (botan_mp_init(&public_x) || botan_mp_init(&public_y) ||
-        botan_mp_from_bin(public_x, &point_bytes[1], sign_half_len) ||
-        botan_mp_from_bin(public_y, &point_bytes[1 + sign_half_len], sign_half_len)) {
+        botan_mp_from_bin(public_x, &point_bytes[1], curve_order) ||
+        botan_mp_from_bin(public_y, &point_bytes[1 + curve_order], curve_order)) {
         goto end;
     }
 
-    const char *curve_name = ec_curves[pubkey->curve].botan_name;
-    if (botan_pubkey_load_ecdsa(&pub, public_x, public_y, curve_name)) {
+    if (botan_pubkey_load_ecdsa(&pub, public_x, public_y, curve->botan_name)) {
         RNP_LOG("Failed to load public key");
         goto end;
     }
@@ -293,19 +304,21 @@ pgp_ecdsa_verify_hash(const pgp_ecc_sig_t *   sign,
         goto end;
     }
 
-    if (botan_pk_op_verify_update(verifier, hash, hash_len)) {
+    const size_t leftmost_bytes = hash_len > curve_order ? curve_order : hash_len;
+    if (botan_pk_op_verify_update(
+          verifier, &hash[hash_len - leftmost_bytes], leftmost_bytes)) {
         goto end;
     }
 
-    if ((BN_num_bytes(sign->r) > sign_half_len) || (BN_num_bytes(sign->s) > sign_half_len) ||
-        (sign_half_len > MAX_CURVE_BYTELEN)) {
+    if ((BN_num_bytes(sign->r) > curve_order) || (BN_num_bytes(sign->s) > curve_order) ||
+        (curve_order > MAX_CURVE_BYTELEN)) {
         goto end;
     }
 
-    BN_bn2bin(sign->r, &sign_buf[sign_half_len - BN_num_bytes(sign->r)]);
-    BN_bn2bin(sign->s, &sign_buf[sign_half_len + sign_half_len - BN_num_bytes(sign->s)]);
+    BN_bn2bin(sign->r, &sign_buf[curve_order - BN_num_bytes(sign->r)]);
+    BN_bn2bin(sign->s, &sign_buf[curve_order + curve_order - BN_num_bytes(sign->s)]);
 
-    ret = botan_pk_op_verify_finish(verifier, sign_buf, sign_half_len * 2) ?
+    ret = botan_pk_op_verify_finish(verifier, sign_buf, curve_order * 2) ?
             PGP_E_V_BAD_SIGNATURE :
             PGP_E_OK;
 
