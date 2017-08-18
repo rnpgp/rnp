09647ffa9453b3f41184b20fc0b2fa63deda80ba
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-08-18 13:47:34 -0400

    Fix secret subkey loading and add a simple test.
    Ref: #368

--- src/tests/rnp_tests.c
+++ src/tests/rnp_tests.c
@@ -157,6 +157,7 @@ main(int argc, char *argv[])
       cmocka_unit_test(sm2_roundtrip),
       cmocka_unit_test(test_load_v3_keyring_pgp),
       cmocka_unit_test(test_load_v4_keyring_pgp),
+      cmocka_unit_test(test_load_keyring_and_count_pgp),
       cmocka_unit_test(pgp_compress_roundtrip)};
 
     /* Each test entry will invoke setup_test before running
