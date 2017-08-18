83d9297eefda2d3d9d54e00910af75919ce338e1
Krzysztof Kwiatkowski <contact@amongbytes.com>
2017-08-18 23:26:48 +0100

    Moves all defines from RFC to rnp_repgp_def.h

--- include/repgp/rnp_repgp_def.h
+++ include/repgp/rnp_repgp_def.h
@@ -1,6 +1,116 @@
 #ifndef __RNP_REPGP_DEF__
 #define __RNP_REPGP_DEF__
 
+/************************************/
+/* Packet Tags - RFC4880, 4.2 */
+/************************************/
+
+/** Packet Tag - Bit 7 Mask (this bit is always set).
+ * The first byte of a packet is the "Packet Tag".  It always
+ * has bit 7 set.  This is the mask for it.
+ *
+ * \see RFC4880 4.2
+ */
+#define PGP_PTAG_ALWAYS_SET 0x80
+
+/** Packet Tag - New Format Flag.
+ * Bit 6 of the Packet Tag is the packet format indicator.
+ * If it is set, the new format is used, if cleared the
+ * old format is used.
+ *
+ * \see RFC4880 4.2
+ */
+#define PGP_PTAG_NEW_FORMAT 0x40
+
+/** Old Packet Format: Mask for content tag.
+ * In the old packet format bits 5 to 2 (including)
+ * are the content tag.  This is the mask to apply
+ * to the packet tag.  Note that you need to
+ * shift by #PGP_PTAG_OF_CONTENT_TAG_SHIFT bits.
+ *
+ * \see RFC4880 4.2
+ */
+#define PGP_PTAG_OF_CONTENT_TAG_MASK 0x3c
+/** Old Packet Format: Offset for the content tag.
+ * As described at #PGP_PTAG_OF_CONTENT_TAG_MASK the
+ * content tag needs to be shifted after being masked
+ * out from the Packet Tag.
+ *
+ * \see RFC4880 4.2
+ */
+#define PGP_PTAG_OF_CONTENT_TAG_SHIFT 2
+/** Old Packet Format: Mask for length type.
+ * Bits 1 and 0 of the packet tag are the length type
+ * in the old packet format.
+ *
+ * See #pgp_ptag_of_lt_t for the meaning of the values.
+ *
+ * \see RFC4880 4.2
+ */
+#define PGP_PTAG_OF_LENGTH_TYPE_MASK 0x03
+
+/**
+ * Maximal length of the OID in hex representation.
+ *
+ * \see RFC4880 bis01 - 9.2 ECC Curve OID
+ */
+#define MAX_CURVE_OID_HEX_LEN 9U
+
+/* Maximum block size for symmetric crypto */
+#define PGP_MAX_BLOCK_SIZE 16
+
+/* Maximum key size for symmetric crypto */
+#define PGP_MAX_KEY_SIZE 32
+
+/* Salt size for hashing */
+#define PGP_SALT_SIZE 8
+
+/** Old Packet Format Lengths.
+ * Defines the meanings of the 2 bits for length type in the
+ * old packet format.
+ *
+ * \see RFC4880 4.2.1
+ */
+typedef enum {
+    PGP_PTAG_OLD_LEN_1 = 0x00,            /* Packet has a 1 byte length -
+                                           * header is 2 bytes long. */
+    PGP_PTAG_OLD_LEN_2 = 0x01,            /* Packet has a 2 byte length -
+                                           * header is 3 bytes long. */
+    PGP_PTAG_OLD_LEN_4 = 0x02,            /* Packet has a 4 byte
+                                           * length - header is 5 bytes
+                                           * long. */
+    PGP_PTAG_OLD_LEN_INDETERMINATE = 0x03 /* Packet has a
+                                           * indeterminate length. */
+} pgp_ptag_of_lt_t;
+
+/** New Packet Format: Mask for content tag.
+ * In the new packet format the 6 rightmost bits
+ * are the content tag.  This is the mask to apply
+ * to the packet tag.  Note that you need to
+ * shift by #PGP_PTAG_NF_CONTENT_TAG_SHIFT bits.
+ *
+ * \see RFC4880 4.2
+ */
+#define PGP_PTAG_NF_CONTENT_TAG_MASK 0x3f
+/** New Packet Format: Offset for the content tag.
+ * As described at #PGP_PTAG_NF_CONTENT_TAG_MASK the
+ * content tag needs to be shifted after being masked
+ * out from the Packet Tag.
+ *
+ * \see RFC4880 4.2
+ */
+#define PGP_PTAG_NF_CONTENT_TAG_SHIFT 0
+
+#define MDC_PKT_TAG 0xd3
+
+enum {
+    PGP_REVOCATION_NO_REASON = 0,
+    PGP_REVOCATION_SUPERSEDED = 1,
+    PGP_REVOCATION_COMPROMISED = 2,
+    PGP_REVOCATION_RETIRED = 3,
+    PGP_REVOCATION_NO_LONGER_VALID = 0x20
+};
+
 /* PTag Content Tags */
 /***************************/
 
@@ -132,5 +242,194 @@ typedef enum {
     PGP_PARSER_ERRCODE = 0x500 + 1 /* Internal Use: Parser Error
                                     * with errcode returned */
 } pgp_content_enum;
+
+/** Public Key Algorithm Numbers.
+ * OpenPGP assigns a unique Algorithm Number to each algorithm that is part of OpenPGP.
+ *
+ * This lists algorithm numbers for public key algorithms.
+ *
+ * \see RFC4880 9.1
+ */
+typedef enum {
+    PGP_PKA_NOTHING = 0,          /* No PKA */
+    PGP_PKA_RSA = 1,              /* RSA (Encrypt or Sign) */
+    PGP_PKA_RSA_ENCRYPT_ONLY = 2, /* RSA Encrypt-Only (deprecated -
+                                   * \see RFC4880 13.5) */
+    PGP_PKA_RSA_SIGN_ONLY = 3,    /* RSA Sign-Only (deprecated -
+                                   * \see RFC4880 13.5) */
+    PGP_PKA_ELGAMAL = 16,         /* Elgamal (Encrypt-Only) */
+    PGP_PKA_DSA = 17,             /* DSA (Digital Signature Algorithm) */
+    PGP_PKA_ECDH = 18,            /* ECDH public key algorithm */
+    PGP_PKA_ECDSA = 19,           /* ECDSA public key algorithm [FIPS186-3] */
+    PGP_PKA_ELGAMAL_ENCRYPT_OR_SIGN =
+      20,                     /* Deprecated. Reserved (formerly Elgamal Encrypt or Sign) */
+    PGP_PKA_RESERVED_DH = 21, /* Reserved for Diffie-Hellman
+                               * (X9.42, as defined for
+                               * IETF-S/MIME) */
+    PGP_PKA_EDDSA = 22,       /* EdDSA from draft-ietf-openpgp-rfc4880bis */
+
+    PGP_PKA_SM2_ENCRYPT = 98, /* SM2 encryption */
+    PGP_PKA_SM2 = 99,         /* SM2 signatures */
+
+    PGP_PKA_PRIVATE00 = 100, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE01 = 101, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE02 = 102, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE03 = 103, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE04 = 104, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE05 = 105, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE06 = 106, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE07 = 107, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE08 = 108, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE09 = 109, /* Private/Experimental Algorithm */
+    PGP_PKA_PRIVATE10 = 110  /* Private/Experimental Algorithm */
+} pgp_pubkey_alg_t;
+
+/**
+ * Enumeration of elliptic curves used by PGP.
+ *
+ * \see RFC4880-bis01 9.2. ECC Curve OID
+ *
+ * Values in this enum correspond to order in ec_curve array (in ec.c)
+ */
+typedef enum {
+    PGP_CURVE_UNKNOWN = 0,
+    PGP_CURVE_NIST_P_256,
+    PGP_CURVE_NIST_P_384,
+    PGP_CURVE_NIST_P_521,
+    PGP_CURVE_ED25519,
+
+    PGP_CURVE_SM2_P_256,
+
+    // Keep always last one
+    PGP_CURVE_MAX
+} pgp_curve_t;
+
+/** Symmetric Key Algorithm Numbers.
+ * OpenPGP assigns a unique Algorithm Number to each algorithm that is
+ * part of OpenPGP.
+ *
+ * This lists algorithm numbers for symmetric key algorithms.
+ *
+ * \see RFC4880 9.2
+ */
+typedef enum {
+    PGP_SA_PLAINTEXT = 0,     /* Plaintext or unencrypted data */
+    PGP_SA_IDEA = 1,          /* IDEA */
+    PGP_SA_TRIPLEDES = 2,     /* TripleDES */
+    PGP_SA_CAST5 = 3,         /* CAST5 */
+    PGP_SA_BLOWFISH = 4,      /* Blowfish */
+    PGP_SA_AES_128 = 7,       /* AES with 128-bit key (AES) */
+    PGP_SA_AES_192 = 8,       /* AES with 192-bit key */
+    PGP_SA_AES_256 = 9,       /* AES with 256-bit key */
+    PGP_SA_TWOFISH = 10,      /* Twofish with 256-bit key (TWOFISH) */
+    PGP_SA_CAMELLIA_128 = 11, /* Camellia with 128-bit key (CAMELLIA) */
+    PGP_SA_CAMELLIA_192 = 12, /* Camellia with 192-bit key */
+    PGP_SA_CAMELLIA_256 = 13, /* Camellia with 256-bit key */
+
+    PGP_SA_SM4 = 105 /* RNP extension - SM4 */
+} pgp_symm_alg_t;
+
+typedef enum {
+    PGP_CIPHER_MODE_NONE = 0,
+    PGP_CIPHER_MODE_CFB = 1,
+    PGP_CIPHER_MODE_CBC = 2,
+    PGP_CIPHER_MODE_OCB = 3,
+} pgp_cipher_mode_t;
+
+/** s2k_usage_t
+ */
+typedef enum {
+    PGP_S2KU_NONE = 0,
+    PGP_S2KU_ENCRYPTED_AND_HASHED = 254,
+    PGP_S2KU_ENCRYPTED = 255
+} pgp_s2k_usage_t;
+
+/** s2k_specifier_t
+ */
+typedef enum {
+    PGP_S2KS_SIMPLE = 0,
+    PGP_S2KS_SALTED = 1,
+    PGP_S2KS_ITERATED_AND_SALTED = 3
+} pgp_s2k_specifier_t;
+
+/** Signature Type.
+ * OpenPGP defines different signature types that allow giving
+ * different meanings to signatures.  Signature types include 0x10 for
+ * generitc User ID certifications (used when Ben signs Weasel's key),
+ * Subkey binding signatures, document signatures, key revocations,
+ * etc.
+ *
+ * Different types are used in different places, and most make only
+ * sense in their intended location (for instance a subkey binding has
+ * no place on a UserID).
+ *
+ * \see RFC4880 5.2.1
+ */
+typedef enum {
+    PGP_SIG_BINARY = 0x00,     /* Signature of a binary document */
+    PGP_SIG_TEXT = 0x01,       /* Signature of a canonical text document */
+    PGP_SIG_STANDALONE = 0x02, /* Standalone signature */
+
+    PGP_CERT_GENERIC = 0x10,  /* Generic certification of a User ID and
+                               * Public Key packet */
+    PGP_CERT_PERSONA = 0x11,  /* Persona certification of a User ID and
+                               * Public Key packet */
+    PGP_CERT_CASUAL = 0x12,   /* Casual certification of a User ID and
+                               * Public Key packet */
+    PGP_CERT_POSITIVE = 0x13, /* Positive certification of a
+                               * User ID and Public Key packet */
+
+    PGP_SIG_SUBKEY = 0x18,  /* Subkey Binding Signature */
+    PGP_SIG_PRIMARY = 0x19, /* Primary Key Binding Signature */
+    PGP_SIG_DIRECT = 0x1f,  /* Signature directly on a key */
+
+    PGP_SIG_REV_KEY = 0x20,    /* Key revocation signature */
+    PGP_SIG_REV_SUBKEY = 0x28, /* Subkey revocation signature */
+    PGP_SIG_REV_CERT = 0x30,   /* Certification revocation signature */
+
+    PGP_SIG_TIMESTAMP = 0x40, /* Timestamp signature */
+
+    PGP_SIG_3RD_PARTY = 0x50 /* Third-Party Confirmation signature */
+} pgp_sig_type_t;
+
+/** Key Flags
+ *
+ * \see RFC4880 5.2.3.21
+ */
+typedef enum {
+    PGP_KF_CERTIFY = 0x01,         /* This key may be used to certify other keys. */
+    PGP_KF_SIGN = 0x02,            /* This key may be used to sign data. */
+    PGP_KF_ENCRYPT_COMMS = 0x04,   /* This key may be used to encrypt communications. */
+    PGP_KF_ENCRYPT_STORAGE = 0x08, /* This key may be used to encrypt storage. */
+    PGP_KF_SPLIT = 0x10,           /* The private component of this key may have been split
+                                            by a secret-sharing mechanism. */
+    PGP_KF_AUTH = 0x20,            /* This key may be used for authentication. */
+    PGP_KF_SHARED = 0x80,          /* The private component of this key may be in the
+                                            possession of more than one person. */
+    /* pseudo flags */
+    PGP_KF_NONE = 0x00,
+    PGP_KF_ENCRYPT = PGP_KF_ENCRYPT_COMMS | PGP_KF_ENCRYPT_STORAGE,
+} pgp_key_flags_t;
+
+/** Types of Compression */
+typedef enum {
+    PGP_C_NONE = 0,
+    PGP_C_ZIP = 1,
+    PGP_C_ZLIB = 2,
+    PGP_C_BZIP2 = 3
+} pgp_compression_type_t;
+
+enum { PGP_SE_IP_DATA_VERSION = 1, PGP_PKSK_V3 = 3 };
+
+/** Version.
+ * OpenPGP has two different protocol versions: version 3 and version 4.
+ *
+ * \see RFC4880 5.2
+ */
+typedef enum {
+    PGP_V2 = 2, /* Version 2 (essentially the same as v3) */
+    PGP_V3 = 3, /* Version 3 */
+    PGP_V4 = 4  /* Version 4 */
+} pgp_version_t;
 
 #endif
