618cd83ddfa313fdb4879b8a705324316792597c
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-09-26 19:46:13 -0400

    Adjust the keyring interfaces, add some other potential interfaces.
    Note: This won't build or anything, we're just throwing ideas around.

--- include/rnp/rnp2.h
+++ include/rnp/rnp2.h
@@ -37,12 +37,13 @@ extern "C" {
 */
 typedef uint32_t rnp_result_t;
 
-const char* rnp_result_to_string(rnp_result_t result);
+const char *rnp_result_to_string(rnp_result_t result);
 
 /*
-* Opaque structures for key types
+* Opaque structures
 */
 typedef struct rnp_keyring_st *rnp_keyring_t;
+typedef struct rnp_key_st *    rnp_key_t;
 
 /**
 * Callback used for getting a passphrase.
@@ -60,52 +61,102 @@ typedef int (*rnp_passphrase_cb)(void *      app_ctx,
 
 /* Operations on key rings */
 
-rnp_result_t rnp_keyring_open(rnp_keyring_t *   keyring,
-                              const char *      keyring_format,
-                              const char *      pub_path,
-                              const char *      sec_path,
-                              rnp_passphrase_cb cb,
-                              void *            app_ctx);
+/** load keyrings from a home directory
+ *
+ * @param secring keyring that will hold the secret keys
+ *        (from secring.gpg, private-keys-v1.d, etc.)
+ * @param pubring keyring that will hold the public keys
+ *        (from pubring.gpg, pubring.kbx, etc.)
+ * @param format the format of the keyring (GPG or GPG21), or NULL to guess
+ * @param path the path to the directory to load from (example: /home/foo/.rnp),
+ *        or NULL to determine it automatically based on the current user.
+ * @return 0 on success, or any other value on error
+ */
+rnp_result_t rnp_keyring_load_homedir(rnp_keyring_t *secring,
+                                      rnp_keyring_t *pubring,
+                                      const char *   format,
+                                      const char *   path);
 
-rnp_result_t rnp_keyring_close(rnp_keyring_t keyring);
+/** load a keyring
+ *
+ * @param ring the keyring
+ * @param format the format of the keyring (GPG or GPG21), or NULL to guess
+ * @param buf
+ * @param buf_len
+ * @return 0 on success, or any other value on error
+ */
+rnp_result_t rnp_keyring_load(rnp_keyring_t *ring,
+                              const char *   format,
+                              const uint8_t  buf[],
+                              size_t         buf_len);
 
-/**
-* Load a public key into the keyring
-* @param key a pointer, on success will be assigned to the new object
-* @param key_bits the binary serialized key
-* @param key_len length of key_bits in bytes
-*/
-rnp_result_t rnp_insert_public_key(rnp_keyring_t keyring,
-                                   const char *  key_format,
-                                   const uint8_t key_bits[],
-                                   size_t        key_len);
+rnp_key_t *rnp_keyring_find_key(rnp_keyring_t ring, const char *identifer);
+rnp_result_t rnp_keyring_add_key(rnp_keyring_t ring, rnp_key_t key);
 
-/**
-* Load an armored public key into the keyring
-* @param key a pointer, on success will be assigned to the new object
-* @param key the PGP armored public key
-*/
-rnp_result_t rnp_insert_armored_public_key(rnp_keyring_t keyring, const char *key);
+/** save a keyring to a file
+ *
+ * @param ring the keyring
+ * @param path the path to the file to save to, or NULL to use the path this
+ *        keyring is already associated with (for homedir)
+ * @return 0 on success, or any other value on error
+ */
+rnp_result_t rnp_keyring_save_to_file(rnp_keyring_t ring, const char *path);
 
-/**
-* Generate a key and add it to the keyring
-*/
-rnp_result_t rnp_generate_private_key(rnp_keyring_t keyring,
-                                      const char *  userid,
-                                      const char *  signature_hash,
-                                      const char *  primary_key_algo,
-                                      const char *  primary_key_params,
-                                      const char *  privkey_passphrase,
-                                      uint32_t      primary_expiration,
-                                      const char *  subkey_algo,
-                                      const char *  subkey_params,
-                                      const char *  subkey_passphrase,
-                                      uint32_t      subkey_expiration);
+rnp_result_t rnp_keyring_save_to_mem(rnp_keyring_t ring, uint8_t *buf[], size_t *buf_len);
+rnp_result_t rnp_keyring_free(rnp_keyring_t *ring);
+
+/* TODO: keyring iteration */
+
+/** generate a key (or pair of keys)
+ *
+ * @param primarykey the primary key pointer that will be populated,
+ *        must not be NULL
+ * @param subkey the subkey key pointer that will be populated,
+ *        must not be NULL
+ * @param jsondata the JSON string that describes the key generation,
+ *        must not be NULL
+ * @return 0 on success, or any other value on error
+ */
+rnp_result_t rnp_generate_key_json(rnp_key_t * primarykey,
+                                   rnp_key_t * subkey,
+                                   const char *jsondata);
+
+/* Key operations */
 
 /**
 * Export a public key from the keyring
 */
-rnp_result_t rnp_export_public_key(rnp_keyring_t keyringt, const char *ident, char **output);
+#define RNP_EXPORT_FLAG_ARMORED (1U << 0)
+rnp_result_t rnp_export_public_key(rnp_key_t key,
+                                   uint32_t  flags,
+                                   char **   output,
+                                   size_t *  output_len);
+
+/* TODO: export encrypted secret keys */
+
+rnp_result_t rnp_key_get_primary_uid(rnp_key_t key, char **uid);
+rnp_result_t rnp_key_get_uid_count(rnp_key_t key, size_t *count);
+rnp_result_t rnp_key_get_uid_at(rnp_key_t key, size_t idx, char **uid);
+
+/* TODO: should these be NULL-terminated hex strings, or just
+ the raw data? Document which. */
+rnp_result_t rnp_key_get_fprint(rnp_key_t key, char **fprint);
+rnp_result_t rnp_key_get_keyid(rnp_key_t key, char **keyid);
+rnp_result_t rnp_key_get_grip(rnp_key_t key, char **grip);
+
+rnp_result_t rnp_key_is_locked(rnp_key_t key, bool *result);
+rnp_result_t rnp_key_unlock(rnp_key_t key, rnp_passphrase_cb cb, void *app_ctx);
+
+rnp_result_t rnp_key_is_protected(rnp_key_t key, bool *result);
+rnp_result_t rnp_key_protect(rnp_key_t key, rnp_passphrase_cb cb, void *app_ctx);
+rnp_result_t rnp_key_unprotect(rnp_key_t key, rnp_passphrase_cb cb, void *app_ctx);
+
+rnp_result_t rnp_key_is_primary_key(rnp_key_t key, bool *result);
+rnp_result_t rnp_key_is_subkey(rnp_key_t key, bool *result);
+rnp_result_t rnp_key_is_secret(rnp_key_t key, bool *result);
+rnp_result_t rnp_key_is_public(rnp_key_t key, bool *result);
+
+/* TODO: function to add a userid to a key */
 
 /* Signature/verification operations */
 
