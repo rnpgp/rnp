c91c6c716d8b37b110ed5fbfdcbf949ae6b6829b
Krzysztof Kwiatkowski <contact@amongbytes.com>
2017-11-03 00:55:41 +0000

    (test): Add possibility to select performance tests to run

--- src/tests/cli_perf.py
+++ src/tests/cli_perf.py
@@ -2,13 +2,22 @@
 
 import sys
 import tempfile
-from os import path
-import os
 import shutil
-import subprocess
+import inspect
+import os
+import logging
+from os import path
 from timeit import default_timer as perf_timer
-from cli_common import find_utility, run_proc, run_proc_fast, pswd_pipe, rnp_file_path, random_text, file_text, size_to_readable, raise_err
-import cli_common
+from argparse import ArgumentParser
+from cli_common import (
+    find_utility,
+    run_proc,
+    run_proc_fast,
+    pswd_pipe,
+    rnp_file_path,
+    size_to_readable,
+    raise_err
+)
 
 RNP = ''
 RNPK = ''
@@ -25,20 +34,23 @@ SMALLFILE = 'smalltest.txt'
 LARGEFILE = 'largetest.txt'
 PASSWORD = 'password'
 
-def setup():
+def setup(workdir):
     # Searching for rnp and gnupg
     global RNP, GPG, RNPK, WORKDIR, RNPDIR, GPGDIR, SMALLSIZE, RMWORKDIR
+    logging.basicConfig(stream=sys.stdout, format="%(message)s")
+    logging.getLogger().setLevel(logging.INFO)
+
     RNP = rnp_file_path('src/rnp/rnp')
     RNPK = rnp_file_path('src/rnpkeys/rnpkeys')
     GPG = find_utility('gpg')
     WORKDIR = os.getcwd()
-    if len(sys.argv) > 1:
-        WORKDIR = sys.argv[1]
+    if workdir:
+        WORKDIR = workdir
     elif not '/tmp/' in WORKDIR:
         WORKDIR = tempfile.mkdtemp(prefix = 'rnpptmp')
         RMWORKDIR = True
 
-    print 'Setting up test in {} ...'.format(WORKDIR)
+    logging.debug('Setting up test in {} ...'.format(WORKDIR))
 
     # Creating working directory and populating it with test files
     RNPDIR = path.join(WORKDIR, '.rnp')
@@ -69,8 +81,6 @@ def setup():
     with open(path.join(WORKDIR, LARGEFILE), 'w') as fd:
         for i in range(0, LARGESIZE / 1024 - 1):
             fd.write(st)
-
-    return
 
 def run_iterated(iterations, func, src, dst, *args):
     runtime = 0
@@ -113,8 +123,7 @@ def gpg_decrypt_file(src, dst, keypass):
 
 def print_test_results(fsize, rnptime, gpgtime, operation):
     if not rnptime or not gpgtime:
-        print '{}:TEST FAILED'.format(operation)
-        return
+        logging.info('{}:TEST FAILED'.format(operation))
 
     if fsize == SMALLSIZE:
         rnpruns = 1.0 / rnptime
@@ -123,10 +132,10 @@ def print_test_results(fsize, rnptime, gpgtime, operation):
 
         if rnpruns >= gpgruns:
             percents = (rnpruns - gpgruns) / gpgruns * 100
-            print '{:<30}: RNP is {:>3.0f}% FASTER then GnuPG ({})'.format(operation, percents, runstr)
+            logging.info('{:<30}: RNP is {:>3.0f}% FASTER then GnuPG ({})'.format(operation, percents, runstr))
         else:
             percents = (gpgruns - rnpruns) / gpgruns * 100
-            print '{:<30}: RNP is {:>3.0f}% SLOWER then GnuPG ({})'.format(operation, percents, runstr)
+            logging.info('{:<30}: RNP is {:>3.0f}% SLOWER then GnuPG ({})'.format(operation, percents, runstr))
     else:
         rnpspeed = fsize / 1024.0 / 1024.0 / rnptime
         gpgspeed = fsize / 1024.0 / 1024.0 / gpgtime
@@ -134,12 +143,10 @@ def print_test_results(fsize, rnptime, gpgtime, operation):
 
         if rnpspeed >= gpgspeed:
             percents = (rnpspeed - gpgspeed) / gpgspeed * 100
-            print '{:<30}: RNP is {:>3.0f}% FASTER then GnuPG ({})'.format(operation, percents, spdstr)
+            logging.info('{:<30}: RNP is {:>3.0f}% FASTER then GnuPG ({})'.format(operation, percents, spdstr))
         else:
             percents = (gpgspeed - rnpspeed) / gpgspeed * 100
-            print '{:<30}: RNP is {:>3.0f}% SLOWER then GnuPG ({})'.format(operation, percents, spdstr)
-
-    return
+            logging.info('{:<30}: RNP is {:>3.0f}% SLOWER then GnuPG ({})'.format(operation, percents, spdstr))
 
 def get_file_params(filetype):
     if filetype == 'small':
@@ -152,79 +159,92 @@ def get_file_params(filetype):
     gpgout = path.join(WORKDIR, outfile + '.gpg')
     return (infile, rnpout, gpgout, iterations, fsize)
 
-def run_tests():
+
+class Benchmark(object):
     rnphome = ['--homedir', RNPDIR]
     gpghome = ['--homedir', GPGDIR]
 
-    # Running each operation iteratively for a small and large file(s), calculating the average
-    # 1. Encryption
-    print '#1. Small file symmetric encryption'
-    infile, rnpout, gpgout, iterations, fsize = get_file_params('small')
-    for armor in [False, True]:
-        tmrnp = run_iterated(iterations, rnp_symencrypt_file, infile, rnpout, 'AES128', 0, 'zip', armor)
-        tmgpg = run_iterated(iterations, gpg_symencrypt_file, infile, gpgout, 'AES128', 0, 1, armor)
-        testname = 'ENCRYPT-SMALL-{}'.format('ARMOR' if armor else 'BINARY')
-        print_test_results(fsize, tmrnp, tmgpg, testname)
+    def small_file_symmetric_encryption(self):
+        # Running each operation iteratively for a small and large file(s), calculating the average
+        # 1. Encryption
+        '''
+        Small file symmetric encryption
+        '''
+        infile, rnpout, gpgout, iterations, fsize = get_file_params('small')
+        for armor in [False, True]:
+            tmrnp = run_iterated(iterations, rnp_symencrypt_file, infile, rnpout, 'AES128', 0, 'zip', armor)
+            tmgpg = run_iterated(iterations, gpg_symencrypt_file, infile, gpgout, 'AES128', 0, 1, armor)
+            testname = 'ENCRYPT-SMALL-{}'.format('ARMOR' if armor else 'BINARY')
+            print_test_results(fsize, tmrnp, tmgpg, testname)
 
-    print '#2. Large file symmetric encryption'
-    infile, rnpout, gpgout, iterations, fsize = get_file_params('large')
-    for cipher in ['AES128', 'AES192', 'AES256', 'TWOFISH', 'BLOWFISH', 'CAST5', 'CAMELLIA128', 'CAMELLIA192', 'CAMELLIA256']:
-        tmrnp = run_iterated(iterations, rnp_symencrypt_file, infile, rnpout, cipher, 0, 'zip', False)
-        tmgpg = run_iterated(iterations, gpg_symencrypt_file, infile, gpgout, cipher, 0, 1, False)
-        testname = 'ENCRYPT-{}-BINARY'.format(cipher)
-        print_test_results(fsize, tmrnp, tmgpg, testname)
+    def large_file_symmetric_encryption(self):
+        '''
+        Large file symmetric encryption
+        '''
+        infile, rnpout, gpgout, iterations, fsize = get_file_params('large')
+        for cipher in ['AES128', 'AES192', 'AES256', 'TWOFISH', 'BLOWFISH', 'CAST5', 'CAMELLIA128', 'CAMELLIA192', 'CAMELLIA256']:
+            tmrnp = run_iterated(iterations, rnp_symencrypt_file, infile, rnpout, cipher, 0, 'zip', False)
+            tmgpg = run_iterated(iterations, gpg_symencrypt_file, infile, gpgout, cipher, 0, 1, False)
+            testname = 'ENCRYPT-{}-BINARY'.format(cipher)
+            print_test_results(fsize, tmrnp, tmgpg, testname)
 
-    print '#3. Large file armored encryption'
-    tmrnp = run_iterated(iterations, rnp_symencrypt_file, infile, rnpout, 'AES128', 0, 'zip', True)
-    tmgpg = run_iterated(iterations, gpg_symencrypt_file, infile, gpgout, 'AES128', 0, 1, True)
-    print_test_results(fsize, tmrnp, tmgpg, 'ENCRYPT-LARGE-ARMOR')
+    def large_file_armored_encryption(self):
+        '''
+        Large file armored encryption
+        '''
+        infile, rnpout, gpgout, iterations, fsize = get_file_params('large')
+        tmrnp = run_iterated(iterations, rnp_symencrypt_file, infile, rnpout, 'AES128', 0, 'zip', True)
+        tmgpg = run_iterated(iterations, gpg_symencrypt_file, infile, gpgout, 'AES128', 0, 1, True)
+        print_test_results(fsize, tmrnp, tmgpg, 'ENCRYPT-LARGE-ARMOR')
 
-    print '#4. Small file symmetric decryption'
-    infile, rnpout, gpgout, iterations, fsize = get_file_params('small')
-    inenc = infile + '.enc'
-    for armor in [False, True]:
-        gpg_symencrypt_file(infile, inenc, 'AES', 0, 1, armor)
+    def small_file_symmetric_decryption(self):
+        '''
+        Small file symmetric decryption
+        '''
+        infile, rnpout, gpgout, iterations, fsize = get_file_params('small')
+        inenc = infile + '.enc'
+        for armor in [False, True]:
+            gpg_symencrypt_file(infile, inenc, 'AES', 0, 1, armor)
+            tmrnp = run_iterated(iterations, rnp_decrypt_file, inenc, rnpout)
+            tmgpg = run_iterated(iterations, gpg_decrypt_file, inenc, gpgout, PASSWORD)
+            testname = 'DECRYPT-SMALL-{}'.format('ARMOR' if armor else 'BINARY')
+            print_test_results(fsize, tmrnp, tmgpg, testname)
+            os.remove(inenc)
+
+    def large_file_symmetric_decryption(self):
+        '''
+        Large file symmetric decryption
+        '''
+        infile, rnpout, gpgout, iterations, fsize = get_file_params('large')
+        inenc = infile + '.enc'
+        for cipher in ['AES128', 'AES192', 'AES256', 'TWOFISH', 'BLOWFISH', 'CAST5', 'CAMELLIA128', 'CAMELLIA192', 'CAMELLIA256']:
+            gpg_symencrypt_file(infile, inenc, cipher, 0, 1, False)
+            tmrnp = run_iterated(iterations, rnp_decrypt_file, inenc, rnpout)
+            tmgpg = run_iterated(iterations, gpg_decrypt_file, inenc, gpgout, PASSWORD)
+            testname = 'DECRYPT-{}-BINARY'.format(cipher)
+            print_test_results(fsize, tmrnp, tmgpg, testname)
+            os.remove(inenc)
+
+    def large_file_armored_decryption(self):
+        '''
+        Large file armored decryption
+        '''
+        infile, rnpout, gpgout, iterations, fsize = get_file_params('large')
+        inenc = infile + '.enc'
+        gpg_symencrypt_file(infile, inenc, 'AES128', 0, 1, True)
         tmrnp = run_iterated(iterations, rnp_decrypt_file, inenc, rnpout)
         tmgpg = run_iterated(iterations, gpg_decrypt_file, inenc, gpgout, PASSWORD)
-        testname = 'DECRYPT-SMALL-{}'.format('ARMOR' if armor else 'BINARY')
-        print_test_results(fsize, tmrnp, tmgpg, testname)
+        print_test_results(fsize, tmrnp, tmgpg, 'DECRYPT-LARGE-ARMOR')
         os.remove(inenc)
 
-    print '#5. Large file symmetric decryption'
-    infile, rnpout, gpgout, iterations, fsize = get_file_params('large')
-    inenc = infile + '.enc'
-    for cipher in ['AES128', 'AES192', 'AES256', 'TWOFISH', 'BLOWFISH', 'CAST5', 'CAMELLIA128', 'CAMELLIA192', 'CAMELLIA256']:
-        gpg_symencrypt_file(infile, inenc, cipher, 0, 1, False)
-        tmrnp = run_iterated(iterations, rnp_decrypt_file, inenc, rnpout)
-        tmgpg = run_iterated(iterations, gpg_decrypt_file, inenc, gpgout, PASSWORD)
-        testname = 'DECRYPT-{}-BINARY'.format(cipher)
-        print_test_results(fsize, tmrnp, tmgpg, testname)
-        os.remove(inenc)
-
-    print '#6. Large file armored decryption'
-    gpg_symencrypt_file(infile, inenc, 'AES128', 0, 1, True)
-    tmrnp = run_iterated(iterations, rnp_decrypt_file, inenc, rnpout)
-    tmgpg = run_iterated(iterations, gpg_decrypt_file, inenc, gpgout, PASSWORD)
-    print_test_results(fsize, tmrnp, tmgpg, 'DECRYPT-LARGE-ARMOR')
-    os.remove(inenc)
-
-    # 3. Signing
-    #print '\n#3. Signing\n'
-    # 4. Verification
-    #print '\n#4. Verification\n'
-    # 5. Cleartext signing
-    #print '\n#5. Cleartext signing and verification\n'
-    # 6. Detached signature
-    #print '\n#6. Detached signing and verification\n'
-
-    return
-
-def cleanup():
-    try:
-        shutil.rmtree(WORKDIR)
-    except:
-        pass
-    return
+        # 3. Signing
+        #print '\n#3. Signing\n'
+        # 4. Verification
+        #print '\n#4. Verification\n'
+        # 5. Cleartext signing
+        #print '\n#5. Cleartext signing and verification\n'
+        # 6. Detached signature
+        #print '\n#6. Detached signing and verification\n'
 
 # Usage ./cli_perf.py [working_directory]
 #
@@ -234,10 +254,41 @@ def cleanup():
 # On linux:
 # mkdir -p /tmp/working
 # sudo mount -t tmpfs -o size=512m tmpfs /tmp/working
-# ./cli_perf.py /tmp/working
+# ./cli_perf.py -w /tmp/working
 # sudo umount /tmp/working
 
+
 if __name__ == '__main__':
-    setup()
-    run_tests()
-    cleanup()
+
+    # parse options
+    parser = ArgumentParser(description="RNP benchmarking")
+    parser.add_argument("-b", "--bench", dest="benchmarks",
+                      help="Name of the comma-separated benchmarks to run", metavar="benchmarks")
+    parser.add_argument("-w", "--workdir", dest="workdir",
+                      help="Working directory to use", metavar="workdir")
+    parser.add_argument("-l", "--list", help="Print list of available benchmarks and exit", action="store_true")
+    args = parser.parse_args()
+
+    # get list of benchamrks to run
+    bench_methods = [ x[0] for x in inspect.getmembers(Benchmark,predicate=inspect.ismethod) if not x[0].startswith("_") ]
+
+    if args.list:
+        for name in bench_methods:
+            logging.info(("\t " + name))
+        sys.exit(0)
+
+    if args.benchmarks:
+        bench_methods = filter(lambda x: x in args.benchmarks.split(","), bench_methods)
+
+    # setup operations
+    setup(args.workdir)
+
+    for name in bench_methods:
+        method = getattr(Benchmark, name)
+        logging.info(("\n" + name + "(): " + inspect.getdoc(method)))
+        method(Benchmark())
+
+    try:
+        shutil.rmtree(WORKDIR)
+    except:
+        logging.info(("Cleanup failed"))
