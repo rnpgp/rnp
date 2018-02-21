bb8c93095960cf894b034b0488d219321cb7c782
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2018-02-20 20:29:01 -0500

    ci: Make local CI runs simpler.

--- travis.sh
+++ travis.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-docker run -ti -v $(pwd):/usr/local/rnp --rm travisci/ci-garnet:packer-1490989530 bash -l
+
+[ $# -eq 0 ] && CMD='bash -l' || CMD=$*
+
+docker run \
+  --tty \
+  --interactive \
+  --cap-add SYS_PTRACE \
+  --volume "$(pwd):/usr/local/rnp" \
+  --workdir /usr/local/rnp \
+  --user 2000 \
+  --rm \
+  travisci/ci-garnet:packer-1512502276-986baf0 \
+  $CMD
 
