1b15221924986202dc7a0707847bf99fcd998659
Henry D. Case <kris@amongbytes.com>
2018-03-03 23:30:34 +0000

    Export secret key

--- include/rnp/rnp.h
+++ include/rnp/rnp.h
@@ -82,7 +82,7 @@ bool   rnp_list_keys(rnp_t *, const int);
 bool   rnp_list_keys_json(rnp_t *, char **, const int);
 bool   rnp_find_key(rnp_t *, const char *);
 char * rnp_get_key(rnp_t *, const char *, const char *);
-char * rnp_export_key(rnp_t *, const char *);
+char * rnp_export_key(rnp_t *, const char *, bool);
 int    rnp_import_key(rnp_t *, char *);
 bool   rnp_generate_key(rnp_t *);
 size_t rnp_secret_count(rnp_t *);
