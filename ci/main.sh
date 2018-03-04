bb1d5a307e4b7c57e2bdd9d3375fd8d5c55c1a93
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2018-03-03 20:35:58 -0500

    ci: Expand the matrix to separate cmocka and cli tests.

--- ci/main.sh
+++ ci/main.sh
@@ -4,8 +4,9 @@ set -eux
 [ "$BUILD_MODE" = "style-check" ] && exec ci/style-check.sh
 
 : "${CORES:=2}"
+: "${RNP_TESTS:=all}"
 
-LD_LIBRARY_PATH="${BOTAN_INSTALL}/lib:${CMOCKA_INSTALL}/lib:${JSONC_INSTALL}/lib"
+LD_LIBRARY_PATH="${BOTAN_INSTALL}/lib:${CMOCKA_INSTALL}/lib:${JSONC_INSTALL}/lib:${GPG_INSTALL}/lib"
 
 [ "$BUILD_MODE" = "coverage" ] && CFLAGS+=" -O0 --coverage"
 
@@ -20,9 +21,9 @@ export LD_LIBRARY_PATH CFLAGS
 
 autoreconf -vfi
 ./configure \
-  --with-botan=${BOTAN_INSTALL} \
-  --with-jsonc=${JSONC_INSTALL} \
-  --with-cmocka=${CMOCKA_INSTALL}
+  --with-botan="${BOTAN_INSTALL}" \
+  --with-jsonc="${JSONC_INSTALL}" \
+  --with-cmocka="${CMOCKA_INSTALL}"
 make clean
 make -j${CORES}
 
@@ -30,9 +31,27 @@ make -j${CORES}
 [[ ${COVERITY_SCAN_BRANCH} = 1 ]] && exit 0
 
 cd src/tests
-./rnp_tests
+run_cmocka=false
+run_cli=false
+case "$RNP_TESTS" in
+  cmocka)
+    run_cmocka=true
+    ;;
+  cli)
+    run_cli=true
+    ;;
+  all)
+    run_cmocka=true
+    run_cli=true
+    ;;
+  *) exit 1 ;;
+esac
 
-LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${GPG_INSTALL}/lib"
-export LD_LIBRARY_PATH
-env RNPC_GPG_PATH="${GPG_INSTALL}/bin/gpg" RNPC_GPGCONF_PATH="${GPG_INSTALL}/bin/gpgconf" python2 cli_tests.py -w -v -d
+[[ $run_cmocka = true ]] && ./rnp_tests
+[[ $run_cli = true ]] && \
+  env RNPC_GPG_PATH="${GPG_INSTALL}/bin/gpg" \
+      RNPC_GPGCONF_PATH="${GPG_INSTALL}/bin/gpgconf" \
+      python2 cli_tests.py -w -v -d
+
+exit 0
 
