437a8da1c5669c00e3f0dee6455c3deb7d0cf883
Nickolay Olshevsky <o.nickolay@gmail.com>
2018-04-11 20:11:45 +0300

    Added cmocka test for stream_dump_packets

--- src/tests/rnp_tests.h
+++ src/tests/rnp_tests.h
@@ -167,6 +167,8 @@ void test_stream_key_encrypt(void **state);
 
 void test_stream_key_signatures(void **state);
 
+void test_stream_dumper(void **state);
+
 #define rnp_assert_int_equal(state, a, b)           \
     do {                                            \
         int _rnp_a = (a);                           \
