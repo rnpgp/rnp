rnpgp/rnp

commit b28f6c0f1f0c3f0ed29dcefc9537a88cf273e5f1

Rework key loading to use key provider.
When loading a subkey, for example, the key provider is now used.
With G10, the key provider is also used for requesting the corresponding public key.

src/librekey/key_store_g10.c
@@ -846,12 +846,12 @@ done:
 }
 
 static bool
-g10_parse_seckey(pgp_io_t *       io,
-                 pgp_seckey_t *   seckey,
-                 const uint8_t *  data,
-                 size_t           data_len,
-                 rnp_key_store_t *pubring,
-                 const char *     password)
+g10_parse_seckey(pgp_io_t *                io,
+                 pgp_seckey_t *            seckey,
+                 const uint8_t *           data,
+                 size_t                    data_len,
+                 const char *              password,
+                 const pgp_key_provider_t *key_provider)
 {
     s_exp_t s_exp = {0};
     bool    ret = false;
@@ -965,17 +965,21 @@ g10_parse_seckey(pgp_io_t *       io,
         goto done;
     }
 
-    if (pubring) {
-        uint8_t    grip[PGP_FINGERPRINT_SIZE];
-        pgp_key_t *pubkey = NULL;
-        if (!rnp_key_store_get_key_grip(&seckey->pubkey, grip)) {
+    if (key_provider) {
+        pgp_key_search_t search = {.type = PGP_KEY_SEARCH_GRIP};
+        if (!rnp_key_store_get_key_grip(&seckey->pubkey, search.by.grip)) {
             goto done;
         }
-        if ((pubkey = rnp_key_store_get_key_by_grip(io, pubring, grip))) {
-            pgp_pubkey_t tmp = seckey->pubkey;
-            seckey->pubkey = *pgp_get_pubkey(pubkey);
-            seckey->pubkey.key = tmp.key;
+        pgp_key_t *pubkey = NULL;
+        if (!(pubkey = pgp_request_key(key_provider,
+                                       &(const pgp_key_request_ctx_t){.op = PGP_OP_MERGE_INFO,
+                                                                      .secret = false,
+                                                                      .search = search}))) {
+            goto done;
         }
+        pgp_pubkey_t tmp = seckey->pubkey;
+        seckey->pubkey = *pgp_get_pubkey(pubkey);
+        seckey->pubkey.key = tmp.key;
     }
 
     if (protected) {
@@ -1025,7 +1029,7 @@ g10_decrypt_seckey(const uint8_t *     data,
     }
 
     seckey = calloc(1, sizeof(*seckey));
-    if (!g10_parse_seckey(&io, seckey, data, data_len, NULL, password)) {
+    if (!g10_parse_seckey(&io, seckey, data, data_len, password, NULL)) {
         goto done;
     }
     if (pubkey) {
@@ -1045,40 +1049,40 @@ done:
 
 bool
 rnp_key_store_g10_from_mem(pgp_io_t *       io,
-                           rnp_key_store_t *pubring,
                            rnp_key_store_t *key_store,
-                           pgp_memory_t *   memory)
+                           pgp_memory_t *   memory,
+                           const pgp_key_provider_t *key_provider)
 {
-    pgp_key_t *key = NULL;
+    pgp_key_t  key = {0};
     bool       ret = false;
 
     pgp_keydata_key_t keydata = {{0}};
-    if (!g10_parse_seckey(io, &keydata.seckey, memory->buf, memory->length, pubring, NULL)) {
+    if (!g10_parse_seckey(io, &keydata.seckey, memory->buf, memory->length, NULL, key_provider)) {
         goto done;
     }
-    if (!rnp_key_store_add_keydata(io, key_store, &keydata, &key, PGP_PTAG_CT_SECRET_KEY)) {
+    if (!pgp_key_from_keydata(&key, &keydata, PGP_PTAG_CT_SECRET_KEY)) {
         goto done;
     }
-    if (key->packetc != 0) {
-        // shouldn't ever happen, but just in case
+    keydata = (pgp_keydata_key_t){{0}};
+    EXPAND_ARRAY((&key), packet);
+    if (!key.packets) {
         goto done;
     }
-    EXPAND_ARRAY(key, packet);
-    if (!key->packets) {
+    key.packets[0].raw = malloc(memory->length);
+    key.packets[0].length = memory->length;
+    memcpy(key.packets[0].raw, memory->buf, memory->length);
+    key.packetc++;
+    key.format = G10_KEY_STORE;
+    key.is_protected = keydata.seckey.encrypted;
+    if (!rnp_key_store_add_key(io, key_store, &key)) {
         goto done;
     }
-    key->packets[0].raw = malloc(memory->length);
-    key->packets[0].length = memory->length;
-    memcpy(key->packets[0].raw, memory->buf, memory->length);
-    key->packetc++;
-    key->format = G10_KEY_STORE;
-    key->is_protected = keydata.seckey.encrypted;
     ret = true;
 
 done:
     if (!ret) {
-        pgp_key_free_data(key);
-        rnp_key_store_remove_key(io, key_store, key);
+        pgp_seckey_free(&keydata.seckey);
+        pgp_key_free_data(&key);
     }
     return ret;
 }

include/rekey/rnp_key_store.h
@@ -141,12 +141,15 @@ rnp_key_store_t *rnp_key_store_new(const char *format, const char *path);
 
 bool rnp_key_store_load_keys(rnp_t *rnp, bool loadsecret);
 
-int rnp_key_store_load_from_file(pgp_io_t *io,
+int  rnp_key_store_load_from_file(pgp_io_t *io,
+                                  rnp_key_store_t *,
+                                  const unsigned,
+                                  const pgp_key_provider_t *key_provider);
+bool rnp_key_store_load_from_mem(pgp_io_t *io,
                                  rnp_key_store_t *,
                                  const unsigned,
-                                 rnp_key_store_t *);
-bool rnp_key_store_load_from_mem(
-  pgp_io_t *io, rnp_key_store_t *, const unsigned, rnp_key_store_t *, pgp_memory_t *);
+                                 pgp_memory_t *,
+                                 const pgp_key_provider_t *key_provider);
 
 bool rnp_key_store_write_to_file(pgp_io_t *io, rnp_key_store_t *, const unsigned);
 bool rnp_key_store_write_to_mem(pgp_io_t *io,
@@ -160,9 +163,7 @@ void rnp_key_store_free(rnp_key_store_t *);
 bool rnp_key_store_list(pgp_io_t *, const rnp_key_store_t *, const int);
 bool rnp_key_store_json(pgp_io_t *, const rnp_key_store_t *, json_object *, const int);
 
-bool rnp_key_store_add_key(pgp_io_t *, rnp_key_store_t *, pgp_key_t *);
-bool rnp_key_store_add_keydata(
-  pgp_io_t *, rnp_key_store_t *, pgp_keydata_key_t *, pgp_key_t **, pgp_content_enum);
+pgp_key_t *rnp_key_store_add_key(pgp_io_t *, rnp_key_store_t *, pgp_key_t *);
 
 bool rnp_key_store_remove_key(pgp_io_t *, rnp_key_store_t *, const pgp_key_t *);
 bool rnp_key_store_remove_key_by_id(pgp_io_t *, rnp_key_store_t *, const uint8_t *);

include/repgp/repgp_def.h
@@ -513,6 +513,7 @@ typedef enum pgp_op_t {
     PGP_OP_ENCRYPT_SYM = 8, /* symmetric encryption */
     PGP_OP_VERIFY = 9,      /* signature verification */
     PGP_OP_ADD_USERID = 10, /* adding a userid */
+    PGP_OP_MERGE_INFO = 11, /* merging information from one key to another */
 } pgp_op_t;
 
 /** Hashing Algorithm Numbers.

src/lib/generate-key.c
@@ -69,8 +69,8 @@ load_generated_key(pgp_output_t **    output,
 {
     bool             ok = false;
     pgp_io_t         io = {.errs = stderr, .res = stdout, .outs = stdout};
-    rnp_key_store_t *pubring = NULL;
     rnp_key_store_t *key_store = NULL;
+    list             key_ptrs = NULL; /* holds primary and pubkey, when used */
 
     // this should generally be zeroed
     assert(dst->type == 0);
@@ -88,27 +88,34 @@ load_generated_key(pgp_output_t **    output,
     }
 
     // if this is a subkey, add the primary in first
-    if (primary_key && !rnp_key_store_add_key(&io, key_store, primary_key)) {
+    if (primary_key && !list_append(&key_ptrs, &primary_key, sizeof(primary_key))) {
         goto end;
     }
     switch (format) {
     case GPG_KEY_STORE:
     case KBX_KEY_STORE:
-        if (!rnp_key_store_pgp_read_from_mem(&io, key_store, 0, *mem)) {
+        if (!rnp_key_store_pgp_read_from_mem(
+              &io,
+              key_store,
+              0,
+              *mem,
+              &(pgp_key_provider_t){.callback = rnp_key_provider_key_ptr_list,
+                                    .userdata = key_ptrs})) {
             RNP_LOG("failed to read back generated key");
             goto end;
         }
         break;
     case G10_KEY_STORE: {
-        // G10 needs the pubring for copying some attributes (key version, creation time, etc)
-        pubring = calloc(1, sizeof(*pubring));
-        if (!pubring) {
+        // G10 needs the pubkey for copying some attributes (key version, creation time, etc)
+        if (!list_append(&key_ptrs, &pubkey, sizeof(pubkey))) {
             goto end;
         }
-        if (!rnp_key_store_add_key(&io, pubring, pubkey)) {
-            goto end;
-        }
-        if (!rnp_key_store_g10_from_mem(&io, pubring, key_store, *mem)) {
+        if (!rnp_key_store_g10_from_mem(
+              &io,
+              key_store,
+              *mem,
+              &(pgp_key_provider_t){.callback = rnp_key_provider_key_ptr_list,
+                                    .userdata = key_ptrs})) {
             goto end;
         }
     } break;
@@ -116,13 +123,9 @@ load_generated_key(pgp_output_t **    output,
         RNP_LOG("invalid key format %d", format);
         break;
     }
-    // if this is a subkey, go ahead and remove the primary added in earlier
-    if (primary_key) {
-        // if a primary key is provided, it should match the sub with regards to type
-        assert(pgp_is_key_secret(primary_key) == pgp_is_key_secret((pgp_key_t*)list_back(key_store->keys)));
-        *primary_key = *(pgp_key_t*)list_front(key_store->keys);
-        rnp_key_store_remove_key(&io, key_store, (pgp_key_t*)list_front(key_store->keys));
-    }
+    // if a primary key is provided, it should match the sub with regards to type
+    assert(!primary_key || (pgp_is_key_secret(primary_key) ==
+                            pgp_is_key_secret((pgp_key_t *) list_back(key_store->keys))));
     if (list_length(key_store->keys) != 1) {
         goto end;
     }
@@ -133,13 +136,10 @@ load_generated_key(pgp_output_t **    output,
 
 end:
     rnp_key_store_free(key_store);
-    if (pubring) {
-        rnp_key_store_remove_key(&io, pubring, (pgp_key_t *) list_front(pubring->keys));
-        rnp_key_store_free(pubring);
-    }
     pgp_teardown_memory_write(*output, *mem);
     *output = NULL;
     *mem = NULL;
+    list_destroy(&key_ptrs);
     return ok;
 }
 
@@ -564,7 +564,7 @@ pgp_generate_subkey(rnp_keygen_subkey_desc_t *     desc,
         break;
     }
     // load the secret key back in
-    if (!load_generated_key(&output, &mem, subkey_sec, secformat, primary_sec, primary_pub)) {
+    if (!load_generated_key(&output, &mem, subkey_sec, secformat, primary_sec, subkey_pub)) {
         goto end;
     }
 

src/lib/pgp-key.c
@@ -55,6 +55,7 @@
 #include <librekey/key_store_pgp.h>
 #include <librekey/key_store_g10.h>
 #include "crypto/s2k.h"
+#include "fingerprint.h"
 
 #include <rnp/rnp_sdk.h>
 #include <librepgp/validate.h>
@@ -116,6 +117,22 @@ pgp_key_t *
 pgp_key_new(void)
 {
     return calloc(1, sizeof(pgp_key_t));
+}
+
+bool
+pgp_key_from_keydata(pgp_key_t *key, pgp_keydata_key_t *keydata, const pgp_content_enum tag)
+{
+    assert(!key->key.pubkey.version);
+    assert(tag == PGP_PTAG_CT_PUBLIC_KEY || tag == PGP_PTAG_CT_PUBLIC_SUBKEY ||
+           tag == PGP_PTAG_CT_SECRET_KEY || tag == PGP_PTAG_CT_SECRET_SUBKEY);
+    if (!pgp_keyid(key->keyid, PGP_KEY_ID_SIZE, &keydata->pubkey) ||
+        !pgp_fingerprint(&key->fingerprint, &keydata->pubkey) ||
+        !rnp_key_store_get_key_grip(&keydata->pubkey, key->grip)) {
+        return false;
+    }
+    key->type = tag;
+    key->key = *keydata;
+    return true;
 }
 
 void
@@ -1099,4 +1116,100 @@ find_suitable_key(pgp_op_t            op,
         subkey_grip = list_next(subkey_grip);
     }
     return NULL;
+}
+
+static const pgp_sig_info_t *
+get_subkey_binding(const pgp_key_t *subkey)
+{
+    // find the subkey binding signature
+    for (unsigned i = 0; i < subkey->subsigc; i++) {
+        const pgp_sig_info_t *sig = &subkey->subsigs[i].sig.info;
+
+        if (sig->type == PGP_SIG_SUBKEY) {
+            return sig;
+        }
+    }
+    return NULL;
+}
+
+static pgp_key_t *
+find_signer(pgp_io_t *                io,
+            const pgp_sig_info_t *    sig,
+            const rnp_key_store_t *   store,
+            const pgp_key_provider_t *key_provider, bool secret)
+{
+    pgp_key_search_t search;
+    pgp_key_t *key = NULL;
+
+    // prefer using the issuer fingerprint when available
+    if (sig->signer_fpr.length) {
+        search.type = PGP_KEY_SEARCH_FINGERPRINT;
+        search.by.fingerprint.length = sig->signer_fpr.length;
+        memcpy(search.by.fingerprint.fingerprint,
+               sig->signer_fpr.fingerprint,
+               sig->signer_fpr.length);
+        // search the store, if provided
+        if (store && (key = rnp_key_store_search(io, store, &search, NULL)) &&
+            pgp_is_key_secret(key) == secret) {
+            return key;
+        }
+        // try the key provider
+        if ((key = pgp_request_key(key_provider,
+                                  &(pgp_key_request_ctx_t){.op = PGP_OP_MERGE_INFO,
+                                                           .secret = secret,
+                                                           .search = search}))) {
+            return key;
+        }
+    }
+    if (sig->signer_id_set) {
+        search.type = PGP_KEY_SEARCH_KEYID;
+        memcpy(search.by.keyid, sig->signer_id, PGP_KEY_ID_SIZE);
+        // search the store, if provided
+        if (store && (key = rnp_key_store_search(io, store, &search, NULL)) &&
+            pgp_is_key_secret(key) == secret) {
+            return key;
+        }
+        if ((key = pgp_request_key(key_provider,
+                            &(pgp_key_request_ctx_t){
+                              .op = PGP_OP_MERGE_INFO, .secret = secret, .search = search}
+                            ))) {
+            return key;
+        }
+    }
+    return NULL;
+}
+
+/* Some background related to this function:
+ * Given that
+ * - It doesn't really make sense to support loading a subkey for which no primary is
+ *   available, because:
+ *   - We can't verify the binding signature without the primary.
+ *   - The primary holds the userids.
+ *   - The way we currently write keyrings out, orphan keys would be omitted.
+ * - The way we maintain a link between primary and sub is via:
+ *   - primary_grip in the subkey
+ *   - subkey_grips in the primary
+ *
+ * We clearly need the primary to be available when loading a subkey.
+ * Rather than requiring it to be loaded first, we just use the key provider.
+ */
+pgp_key_t *
+pgp_get_primary_key_for(pgp_io_t *                io,
+                         const pgp_key_t *         subkey,
+                         const rnp_key_store_t *   store,
+                         const pgp_key_provider_t *key_provider)
+{
+    const pgp_sig_info_t *binding_sig = NULL;
+
+    // find the subkey binding signature
+    binding_sig = get_subkey_binding(subkey);
+    if (!binding_sig) {
+        RNP_LOG_FD(io->errs, "Missing subkey binding signature for key.");
+        return NULL;
+    }
+    if (!binding_sig->signer_fpr.length && !binding_sig->signer_id_set) {
+        RNP_LOG_FD(io->errs, "No issuer information in subkey binding signature.");
+        return NULL;
+    }
+    return find_signer(io, binding_sig, store, key_provider, pgp_is_key_secret(subkey));
 }

src/lib/pgp-key.h
@@ -55,7 +55,6 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "pass-provider.h"
-#include "key-provider.h"
 #include <repgp/repgp.h>
 #include <rekey/rnp_key_store.h>
 #include "memory.h"
@@ -87,6 +86,15 @@ struct pgp_key_t {
 };
 
 struct pgp_key_t *pgp_key_new(void);
+
+/** create a key from pgp_keydata_key_t
+ *
+ *  This sets up basic properties of the key like keyid/fpr/grip, type, etc.
+ *  It does not set primary_grip or subkey_grips (the key store does this).
+ */
+bool pgp_key_from_keydata(pgp_key_t *            key,
+                          pgp_keydata_key_t *    keydata,
+                          const pgp_content_enum tag);
 
 /** free the internal data of a key *and* the key structure itself
  *
@@ -257,5 +265,10 @@ pgp_key_t *find_suitable_key(pgp_op_t            op,
                              pgp_key_t *         key,
                              pgp_key_provider_t *key_provider,
                              uint8_t             desired_usage);
+
+pgp_key_t *pgp_get_primary_key_for(pgp_io_t *                io,
+                                   const pgp_key_t *         subkey,
+                                   const rnp_key_store_t *   store,
+                                   const pgp_key_provider_t *key_provider);
 
 #endif // RNP_PACKET_KEY_H

src/lib/rnp.c
@@ -940,7 +940,7 @@ rnp_import_key(rnp_t *rnp, char *f)
     }
 
     // load the key(s)
-    if (!rnp_key_store_load_from_file(rnp->io, tmp_keystore, realarmor, rnp->pubring)) {
+    if (!rnp_key_store_load_from_file(rnp->io, tmp_keystore, realarmor, &rnp->key_provider)) {
         RNP_LOG("failed to load key from file %s", f);
         goto done;
     }

src/lib/rnp2.c
@@ -60,14 +60,15 @@ struct rnp_key_handle_st {
 };
 
 struct rnp_ffi_st {
-    pgp_io_t         io;
-    rnp_key_store_t *pubring;
-    rnp_key_store_t *secring;
-    rnp_get_key_cb   getkeycb;
-    void *           getkeycb_ctx;
-    rnp_password_cb  getpasscb;
-    void *           getpasscb_ctx;
-    rng_t            rng;
+    pgp_io_t           io;
+    rnp_key_store_t *  pubring;
+    rnp_key_store_t *  secring;
+    rnp_get_key_cb     getkeycb;
+    void *             getkeycb_ctx;
+    rnp_password_cb    getpasscb;
+    void *             getpasscb_ctx;
+    rng_t              rng;
+    pgp_key_provider_t key_provider;
 };
 
 struct rnp_input_st {
@@ -159,7 +160,7 @@ static pgp_key_t *
 get_key_require_secret(rnp_key_handle_t handle);
 
 static pgp_key_t *
-key_provider_bounce(const pgp_key_request_ctx_t *ctx, void *userdata)
+ffi_key_provider(const pgp_key_request_ctx_t *ctx, void *userdata)
 {
     rnp_ffi_t        ffi = (rnp_ffi_t) userdata;
     rnp_key_store_t *store = ctx->secret ? ffi->secring : ffi->pubring;
@@ -290,6 +291,7 @@ rnp_ffi_create(rnp_ffi_t *ffi, const char *pub_format, const char *sec_format)
         ret = RNP_ERROR_OUT_OF_MEMORY;
         goto done;
     }
+    ob->key_provider = (pgp_key_provider_t){.callback = ffi_key_provider, .userdata = ob};
     if (!rng_init(&ob->rng, RNG_DRBG)) {
         ret = RNP_ERROR_RNG;
         goto done;
@@ -710,6 +712,12 @@ load_keys_from_input(rnp_ffi_t ffi, rnp_input_t input, rnp_key_store_t *store)
     rnp_result_t ret = RNP_ERROR_GENERIC;
     uint8_t *    buf = NULL;
     size_t       buf_len;
+    const pgp_key_provider_t *key_providers[] = {
+      &(pgp_key_provider_t){.callback = rnp_key_provider_store, .userdata = store},
+      &ffi->key_provider,
+      NULL};
+    const pgp_key_provider_t key_provider = {.callback = rnp_key_provider_chained,
+                                             .userdata = key_providers};
 
     if (input->src_directory) {
         // load the keys
@@ -719,10 +727,10 @@ load_keys_from_input(rnp_ffi_t ffi, rnp_input_t input, rnp_key_store_t *store)
             ret = RNP_ERROR_OUT_OF_MEMORY;
             goto done;
         }
-        if (!rnp_key_store_load_from_file(&ffi->io, store, 0, ffi->pubring)) {
+        if (!rnp_key_store_load_from_file(&ffi->io, store, 0, &key_provider)) {
             // for the GPG format, we try again with armored=true
             if (store->format != GPG_KEY_STORE ||
-                !rnp_key_store_load_from_file(&ffi->io, store, 1, ffi->pubring)) {
+                !rnp_key_store_load_from_file(&ffi->io, store, 1, &key_provider)) {
                 ret = RNP_ERROR_BAD_FORMAT;
                 goto done;
             }
@@ -736,10 +744,10 @@ load_keys_from_input(rnp_ffi_t ffi, rnp_input_t input, rnp_key_store_t *store)
         }
         // load the keys
         pgp_memory_t mem = {.buf = buf, .length = buf_len};
-        if (!rnp_key_store_load_from_mem(&ffi->io, store, 0, ffi->pubring, &mem)) {
+        if (!rnp_key_store_load_from_mem(&ffi->io, store, 0, &mem, &key_provider)) {
             // for the GPG format, we try again with armored=true
             if (store->format != GPG_KEY_STORE ||
-                !rnp_key_store_load_from_mem(&ffi->io, store, 1, ffi->pubring, &mem)) {
+                !rnp_key_store_load_from_mem(&ffi->io, store, 1, &mem, &key_provider)) {
                 ret = RNP_ERROR_BAD_FORMAT;
                 goto done;
             }
@@ -1617,8 +1625,7 @@ rnp_op_encrypt_execute(rnp_op_encrypt_t op)
       .password_provider = &provider,
       .ctx = &op->rnpctx,
       .param = NULL,
-      .key_provider =
-        &(pgp_key_provider_t){.callback = key_provider_bounce, .userdata = op->ffi},
+      .key_provider = &op->ffi->key_provider
     };
 
     rnp_result_t ret;
@@ -1802,8 +1809,7 @@ rnp_op_sign_execute(rnp_op_sign_t op)
       .password_provider = &provider,
       .ctx = &op->rnpctx,
       .param = NULL,
-      .key_provider =
-        &(pgp_key_provider_t){.callback = key_provider_bounce, .userdata = op->ffi},
+      .key_provider = &op->ffi->key_provider
     };
 
     for (list_item *sig = list_front(op->signatures); sig; sig = list_next(sig)) {
@@ -1951,8 +1957,7 @@ rnp_op_verify_execute(rnp_op_verify_t op)
                                                  .cb_data = op->ffi->getpasscb_ctx}};
     pgp_parse_handler_t handler = {
       .password_provider = &password_provider,
-      .key_provider =
-        &(pgp_key_provider_t){.callback = key_provider_bounce, .userdata = op->ffi},
+      .key_provider = &op->ffi->key_provider,
       .on_signatures = rnp_op_verify_on_signatures,
       .src_provider = rnp_verify_src_provider,
       .dest_provider = rnp_verify_dest_provider,
@@ -2089,7 +2094,7 @@ rnp_decrypt(rnp_ffi_t ffi, rnp_input_t input, rnp_output_t output)
                                                  .cb_data = ffi->getpasscb_ctx}};
     pgp_parse_handler_t handler = {
       .password_provider = &password_provider,
-      .key_provider = &(pgp_key_provider_t){.callback = key_provider_bounce, .userdata = ffi},
+      .key_provider = &ffi->key_provider,
       .dest_provider = rnp_decrypt_dest_provider,
       .param = output,
       .ctx = &rnpctx};

commit ea94aa06fb09fd1f73d9909d0d132fe6c99909af

Fix G10 protected keys incorrectly marked as not protected.
This was a regression I likely introduced in b28f6c0.
Between then and now, protected G10 keys have been unusable because
they were not recognized as protected (and would not request a
password when they should have).

src/librekey/key_store_g10.c
@@ -1063,6 +1063,7 @@ rnp_key_store_g10_from_mem(pgp_io_t *       io,
     if (!pgp_key_from_keydata(&key, &keydata, PGP_PTAG_CT_SECRET_KEY)) {
         goto done;
     }
+    // this data belongs to the key now
     keydata = (pgp_keydata_key_t){{0}};
     EXPAND_ARRAY((&key), packet);
     if (!key.packets) {
@@ -1073,7 +1074,7 @@ rnp_key_store_g10_from_mem(pgp_io_t *       io,
     memcpy(key.packets[0].raw, memory->buf, memory->length);
     key.packetc++;
     key.format = G10_KEY_STORE;
-    key.is_protected = keydata.seckey.encrypted;
+    key.is_protected = key.key.seckey.encrypted;
     if (!rnp_key_store_add_key(io, key_store, &key)) {
         goto done;
     }