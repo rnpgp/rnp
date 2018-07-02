44ba4abf5057af139dde39f5e942d15feb36bef4
Nickolay Olshevsky <o.nickolay@gmail.com>
2018-07-02 20:04:51 +0300

    Extended tests with checks for rnp_key_store_get_key_by_name.

--- src/tests/rnp_tests.cpp
+++ src/tests/rnp_tests.cpp
@@ -173,6 +173,7 @@ main(int argc, char *argv[])
       cmocka_unit_test(test_repgp_verify),
       cmocka_unit_test(test_generated_key_sigs),
       cmocka_unit_test(test_key_store_search),
+      cmocka_unit_test(test_key_store_search_by_name),
       cmocka_unit_test(test_stream_memory),
       cmocka_unit_test(test_stream_signatures),
       cmocka_unit_test(test_stream_key_load),
