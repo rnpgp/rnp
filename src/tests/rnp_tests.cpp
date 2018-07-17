965f4f53120b48bc616f906fb47e16698519e728
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2018-07-17 15:53:36 -0400

    Add test for FFI detached sign w/G10 keys.

--- src/tests/rnp_tests.cpp
+++ src/tests/rnp_tests.cpp
@@ -204,6 +204,7 @@ main(int argc, char *argv[])
       cmocka_unit_test(test_ffi_key_to_json),
       cmocka_unit_test(test_ffi_key_iter),
       cmocka_unit_test(test_ffi_locate_key),
+      cmocka_unit_test(test_ffi_signatures_detached_memory_g10),
       cmocka_unit_test(test_cli_rnp),
     };
 
