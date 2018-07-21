6cf2492206e24c115c89343a6f6996e603d4fb9f
Nickolay Olshevsky <o.nickolay@gmail.com>
2018-07-21 17:18:01 +0300

    Refactor rnp_key_from_transferable_key/subkey to not use temporary keyring; Added rnp_key_add_subkey_grip function which will check for duplicates.

--- src/librekey/key_store_pgp.h
+++ src/librekey/key_store_pgp.h
@@ -80,7 +80,7 @@ bool rnp_key_store_add_transferable_key(rnp_key_store_t *       keyring,
 
 bool rnp_key_from_transferable_key(pgp_key_t *key, pgp_transferable_key_t *tkey);
 
-bool rnp_key_from_transferable_subkey(pgp_key_t *                key,
+bool rnp_key_from_transferable_subkey(pgp_key_t *                subkey,
                                       pgp_transferable_subkey_t *tskey,
                                       pgp_key_t *                primary);
 
