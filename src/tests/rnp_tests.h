d24c91815a2e8bd9fc84cd41e9fc5a172ab33d32
Nickolay Olshevsky <o.nickolay@gmail.com>
2018-07-31 17:34:10 +0300

    Test for successfull key validation

--- src/tests/rnp_tests.h
+++ src/tests/rnp_tests.h
@@ -99,6 +99,8 @@ void test_load_g10(void **state);
 
 void test_key_unlock_pgp(void **state);
 
+void test_key_validate(void **state);
+
 void test_key_protect_load_pgp(void **state);
 
 void test_key_add_userid(void **state);
