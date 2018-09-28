a801124b1b6161bc61c0a6267c74853a91db00fa
Nickolay Olshevsky <o.nickolay@gmail.com>
2018-09-28 14:42:50 +0300

    Dump key packet ids and fingerprints/grips if requested.

--- src/librepgp/stream-dump.h
+++ src/librepgp/stream-dump.h
@@ -37,6 +37,7 @@
 typedef struct rnp_dump_ctx_t {
     bool dump_mpi;
     bool dump_packets;
+    bool dump_grips;
 } rnp_dump_ctx_t;
 
 rnp_result_t stream_dump_packets(rnp_dump_ctx_t *ctx, pgp_source_t *src, pgp_dest_t *dst);
