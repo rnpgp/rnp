c6b30035855e234f267b7e7ad85e9d669e420633
Nickolay Olshevsky <o.nickolay@gmail.com>
2019-01-16 19:04:51 +0300

    Remove repgp.cpp/repgp.h as this interface is not used.

--- src/tests/rnp_tests.h
+++ src/tests/rnp_tests.h
@@ -124,10 +124,6 @@ void test_key_protect_load_pgp(void **state);
 
 void test_key_add_userid(void **state);
 
-void test_repgp_decrypt(void **state);
-
-void test_repgp_verify(void **state);
-
 void test_generated_key_sigs(void **state);
 
 void test_key_store_search(void **state);
