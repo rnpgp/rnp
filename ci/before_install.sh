d2ddc92b1e0d69ef017a1eebc9e591b28757d305
Daniel Wyatt <daniel.wyatt@ribose.com>
2019-02-05 17:30:00 -0500

    ci: Modify get_os to return exact os name.

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 set -ex
 
-# https://gist.github.com/marcusandre/4b88c2428220ea255b83
+# Derived from: https://gist.github.com/marcusandre/4b88c2428220ea255b83
 get_os() {
-  if [ -z $OSTYPE ]; then
-    echo "$(uname | tr '[:upper:]' '[:lower:]')"
-  else
-    echo "$(echo $OSTYPE | tr '[:upper:]' '[:lower:]')"
+  local ostype=$(echo $OSTYPE | tr '[:upper:]' '[:lower:]')
+  if [ -z "$ostype" ]; then
+    ostype=$(uname | tr '[:upper:]' '[:lower:]')
   fi
+
+  case $ostype in
+    freebsd*) echo "freebsd" ;;
+    netbsd*) echo "netbsd" ;;
+    openbsd*) echo "openbsd" ;;
+    darwin*) echo "mac" ;;
+    linux*) echo "linux" ;;
+    cygwin*) echo "cygwin" ;;
+    *) echo "unknown"; exit 1 ;;
+  esac
 }
 
 macos_install() {
