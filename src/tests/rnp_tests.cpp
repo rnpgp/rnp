211e93c342cf2af435c764c48f2b5224e9bbb063
Daniel Wyatt <daniel.wyatt@ribose.com>
2019-03-17 09:27:00 -0400

    Fix double-free with invalid armored headers.
    Fixes #814.

--- src/tests/rnp_tests.cpp
+++ src/tests/rnp_tests.cpp
@@ -197,6 +197,7 @@ main(int argc, char *argv[])
       cmocka_unit_test(test_stream_verify_no_key),
       cmocka_unit_test(test_stream_key_signature_validate),
       cmocka_unit_test(test_stream_key_load_errors),
+      cmocka_unit_test(test_stream_814_dearmor_double_free),
       cmocka_unit_test(test_ffi_homedir),
       cmocka_unit_test(test_ffi_keygen_json_pair),
       cmocka_unit_test(test_ffi_keygen_json_primary),
