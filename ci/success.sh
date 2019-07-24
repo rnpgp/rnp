e323cf090d623c2888dd28b4ede404aa697639dd
Daniel Wyatt <daniel.wyatt@ribose.com>
2019-07-23 20:24:31 -0400

    ci: Fix code coverage.
    This was likely broken when we started doing out-of-source builds.

--- ci/success.sh
+++ ci/success.sh
@@ -2,6 +2,9 @@
 set -eu
 
 if [ "$BUILD_MODE" = "coverage" ]; then
-  bash <(curl -s https://codecov.io/bash)
+  bash <(curl -s https://codecov.io/bash) \
+    -K \
+    -p "${LOCAL_BUILDS}/rnp-build" \
+    -R "$TRAVIS_BUILD_DIR"
 fi
 
