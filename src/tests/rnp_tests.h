c951384d72b1f0d751e8aed8a39c7cf8b8a71f63
Nickolay Olshevsky <o.nickolay@gmail.com>
2019-10-18 12:21:00 +0300

    Add test for rnp_op_verify_get_signature_handle/rnp_signature_packet_to_json()

--- src/tests/rnp_tests.h
+++ src/tests/rnp_tests.h
@@ -204,6 +204,8 @@ void test_ffi_signatures_detached(void **state);
 
 void test_ffi_signatures(void **state);
 
+void test_ffi_signatures_dump(void **state);
+
 void test_ffi_encrypt_pk_key_provider(void **state);
 
 void test_ffi_key_to_json(void **state);
