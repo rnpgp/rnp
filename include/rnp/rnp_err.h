54251e19929ab62e32225ce9a71d40cbadcadc8e
Jan <joke325@gmail.com>
2019-11-23 13:08:57 +0100

    Added an installed header containing RNP_SUCCESS
    The copyright notice has been updated.

--- include/rnp/rnp_err.h
+++ include/rnp/rnp_err.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, [Ribose Inc](https://www.ribose.com).
+ * Copyright (c) 2017-2019, [Ribose Inc](https://www.ribose.com).
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
