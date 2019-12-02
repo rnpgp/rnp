rnpgp/rnp

commit be6b88c36895ec6c80ad8cecdd4492b428f897db

Logging a warning when first partial packet length is less than 512 bytes
Removed extra lines

src/librepgp/stream-parse.cpp
@@ -263,6 +263,10 @@ init_partial_pkt_src(pgp_source_t *src, pgp_source_t *readsrc)
     src->close = partial_pkt_src_close;
     src->type = PGP_STREAM_PARLEN_PACKET;
 
+    if (param->psize < PGP_PARTIAL_PKT_FIRST_PART_MIN_SIZE) {
+        RNP_LOG("first part of partial length packet sequence has size %d and that's less than allowed by the protocol", param->psize);
+    }
+
     return RNP_SUCCESS;
 }
 

src/librepgp/stream-common.h
@@ -34,6 +34,8 @@
 #define PGP_INPUT_CACHE_SIZE 32768
 #define PGP_OUTPUT_CACHE_SIZE 32768
 
+#define PGP_PARTIAL_PKT_FIRST_PART_MIN_SIZE 512
+
 typedef enum {
     PGP_STREAM_NULL,
     PGP_STREAM_FILE,

src/tests/partial-length.cpp
@@ -38,19 +38,6 @@
 #include <vector>
 #include <string>
 
-// pass provider always returns 'password'
-static bool
-pass_provider(rnp_ffi_t         ffi,
-            void *           app_ctx,
-            rnp_key_handle_t key,
-            const char *     pgp_context,
-            char             buf[],
-            size_t           buf_len)
-{
-    strncpy(buf, "password", buf_len);
-    return true;
-}
-
 // structure for filling input
 typedef struct {
     uint32_t      remaining;
@@ -76,18 +63,14 @@ static void
 test_partial_length_init(rnp_ffi_t *ffi, uint32_t key_flags)
 {
     rnp_input_t     input = NULL;
-
-    /* init ffi */
+    // init ffi
     assert_rnp_success(rnp_ffi_create(ffi, "GPG", "GPG"));
-
-    assert_rnp_success(rnp_ffi_set_pass_provider(*ffi, pass_provider, NULL));
-
+    assert_rnp_success(rnp_ffi_set_pass_provider(*ffi, ffi_string_password_provider, (void *) "password"));
     if (key_flags & RNP_LOAD_SAVE_SECRET_KEYS) {
         assert_rnp_success(rnp_input_from_path(&input, "data/keyrings/1/secring.gpg"));
         assert_rnp_success(rnp_load_keys(*ffi, "GPG", input, key_flags));
         assert_rnp_success(rnp_input_destroy(input));
     }
-
     if (key_flags & RNP_LOAD_SAVE_PUBLIC_KEYS) {
         assert_rnp_success(rnp_input_from_path(&input, "data/keyrings/1/pubring.gpg"));
         assert_rnp_success(rnp_load_keys(*ffi, "GPG", input, key_flags));
@@ -99,7 +82,6 @@ TEST_F(rnp_tests, test_partial_length_public_key)
 {
     rnp_input_t     input = NULL;
     rnp_ffi_t       ffi = NULL;
-
     assert_rnp_success(rnp_ffi_create(&ffi, "GPG", "GPG"));
     assert_rnp_success(rnp_input_from_path(&input, "data/test_partial_length/pubring.gpg.partial"));
     assert_int_equal(rnp_load_keys(ffi, "GPG", input, RNP_LOAD_SAVE_PUBLIC_KEYS), RNP_ERROR_BAD_FORMAT);
@@ -112,17 +94,14 @@ TEST_F(rnp_tests, test_partial_length_signature)
     rnp_ffi_t       ffi = NULL;
     rnp_input_t     input = NULL;
     rnp_output_t    output = NULL;
-
     // init ffi
     test_partial_length_init(&ffi, RNP_LOAD_SAVE_PUBLIC_KEYS);
-
     // message having partial length signature packet
     assert_rnp_success(rnp_input_from_path(&input, "data/test_partial_length/message.txt.partial-signed"));
     assert_rnp_success(rnp_output_to_null(&output));
     rnp_op_verify_t verify = NULL;
     assert_rnp_success(rnp_op_verify_create(&verify, ffi, input, output));
     assert_rnp_failure(rnp_op_verify_execute(verify));
-
     // cleanup
     assert_rnp_success(rnp_op_verify_destroy(verify));
     assert_rnp_success(rnp_input_destroy(input));
@@ -135,17 +114,14 @@ TEST_F(rnp_tests, test_partial_length_first_packet_256)
     rnp_ffi_t       ffi = NULL;
     rnp_input_t     input = NULL;
     rnp_output_t    output = NULL;
-
     // init ffi
     test_partial_length_init(&ffi, RNP_LOAD_SAVE_PUBLIC_KEYS);
-
     // message having first partial length packet of 256 bytes
     assert_rnp_success(rnp_input_from_path(&input, "data/test_partial_length/message.txt.partial-256"));
     assert_rnp_success(rnp_output_to_null(&output));
     rnp_op_verify_t verify = NULL;
     assert_rnp_success(rnp_op_verify_create(&verify, ffi, input, output));
     assert_rnp_success(rnp_op_verify_execute(verify));
-
     // cleanup
     assert_rnp_success(rnp_op_verify_destroy(verify));
     assert_rnp_success(rnp_input_destroy(input));
@@ -158,17 +134,14 @@ TEST_F(rnp_tests, test_partial_length_zero_last_chunk)
     rnp_ffi_t       ffi = NULL;
     rnp_input_t     input = NULL;
     rnp_output_t    output = NULL;
-
     // init ffi
     test_partial_length_init(&ffi, RNP_LOAD_SAVE_PUBLIC_KEYS);
-
     // message in partial packets having 0-size last chunk
     assert_rnp_success(rnp_input_from_path(&input, "data/test_partial_length/message.txt.partial-zero-last"));
     assert_rnp_success(rnp_output_to_null(&output));
     rnp_op_verify_t verify = NULL;
     assert_rnp_success(rnp_op_verify_create(&verify, ffi, input, output));
     assert_rnp_success(rnp_op_verify_execute(verify));
-
     // cleanup
     assert_rnp_success(rnp_op_verify_destroy(verify));
     assert_rnp_success(rnp_input_destroy(input));
@@ -181,17 +154,14 @@ TEST_F(rnp_tests, test_partial_length_largest)
     rnp_ffi_t       ffi = NULL;
     rnp_input_t     input = NULL;
     rnp_output_t    output = NULL;
-
     // init ffi
     test_partial_length_init(&ffi, RNP_LOAD_SAVE_PUBLIC_KEYS);
-
     // message having largest possible partial packet
     assert_rnp_success(rnp_input_from_path(&input, "data/test_partial_length/message.txt.partial-1g"));
     assert_rnp_success(rnp_output_to_null(&output));
     rnp_op_verify_t verify = NULL;
     assert_rnp_success(rnp_op_verify_create(&verify, ffi, input, output));
     assert_rnp_success(rnp_op_verify_execute(verify));
-
     // cleanup
     assert_rnp_success(rnp_op_verify_destroy(verify));
     assert_rnp_success(rnp_input_destroy(input));
@@ -206,52 +176,42 @@ TEST_F(rnp_tests, test_partial_length_first_packet_length)
     rnp_output_t        output = NULL;
     rnp_op_sign_t       sign = NULL;
     rnp_key_handle_t    key = NULL;
-
     // uncacheable size will emulate unknown length from callback source
     size_t uncacheable_size = PGP_INPUT_CACHE_SIZE + 1;
-
     // init ffi
     test_partial_length_init(&ffi, RNP_LOAD_SAVE_SECRET_KEYS);
-
     // generate a sequence of octets with appropriate length using callback
     dummy_reader_ctx_st reader_ctx;
     reader_ctx.dummy = 'X';
     reader_ctx.remaining = uncacheable_size;
     assert_rnp_success(rnp_input_from_callback(&input, dummy_reader, NULL, &reader_ctx));
-
     assert_rnp_success(rnp_output_to_memory(&output, uncacheable_size + 1024));
-
     assert_rnp_success(rnp_op_sign_create(&sign, ffi, input, output));
     assert_rnp_success(rnp_locate_key(ffi, "keyid", "7BC6709B15C23A4A", &key));
     assert_rnp_success(rnp_op_sign_add_signature(sign, key, NULL));
-
     assert_rnp_success(rnp_key_handle_destroy(key));
     key = NULL;
-
     // signing
     assert_rnp_success(rnp_op_sign_execute(sign));
-
     // read from the saved packets
     pgp_source_t    src;
     uint8_t         *mem = NULL;
     size_t          len = 0;
     assert_rnp_success(rnp_output_memory_get_buf(output, &mem, &len, false));
     assert_rnp_success(init_mem_src(&src, mem, len, false));
-
     // skip first packet (one-pass signature)
     pgp_packet_body_t body;
     assert_rnp_success(stream_read_packet_body(&src, &body));
     assert_int_equal(body.tag, PGP_PTAG_CT_1_PASS_SIG);
-
+    free_packet_body(&body);
     // checking next packet header (should be partial length literal data)
     uint8_t flags = 0;
     assert_int_equal(src_read(&src, &flags, 1), 1);
     assert_int_equal(flags, PGP_PTAG_ALWAYS_SET | PGP_PTAG_NEW_FORMAT | PGP_PTAG_CT_LITDATA);
     // checking length
     bool last = true; // should be reset by stream_read_partial_chunk_len()
-    assert_true(stream_read_partial_chunk_len(&src, &last) >= 512);
+    assert_true(stream_read_partial_chunk_len(&src, &last) >= PGP_PARTIAL_PKT_FIRST_PART_MIN_SIZE);
     assert_false(last);
-
     // cleanup
     src_close(&src);
     assert_rnp_success(rnp_op_sign_destroy(sign));

commit a72f00e38ad62fb902e3342fe8a2dc0fa6a25101

Fix incorrect format modifier warning introduced in be6b88c3

src/librepgp/stream-parse.cpp
@@ -264,7 +264,7 @@ init_partial_pkt_src(pgp_source_t *src, pgp_source_t *readsrc)
     src->type = PGP_STREAM_PARLEN_PACKET;
 
     if (param->psize < PGP_PARTIAL_PKT_FIRST_PART_MIN_SIZE) {
-        RNP_LOG("first part of partial length packet sequence has size %d and that's less than allowed by the protocol", param->psize);
+        RNP_LOG("first part of partial length packet sequence has size %d and that's less than allowed by the protocol", (int) param->psize);
     }
 
     return RNP_SUCCESS;