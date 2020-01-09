eb52f845218ca7ca5ac64d37c467546466e0ead7
Nickolay Olshevsky <o.nickolay@gmail.com>
2020-01-09 17:03:43 +0200

    Remove key_store_internal.h and unused functions from key_store_pgp.cpp

--- src/librekey/rnp_key_store.cpp
+++ src/librekey/rnp_key_store.cpp
@@ -44,7 +44,6 @@
 #include <rekey/rnp_key_store.h>
 #include <librepgp/stream-packet.h>
 
-#include "key_store_internal.h"
 #include "key_store_pgp.h"
 #include "key_store_kbx.h"
 #include "key_store_g10.h"
@@ -259,75 +258,6 @@ rnp_key_store_write_to_dst(rnp_key_store_t *key_store, pgp_dest_t *dst)
     }
 
     return false;
-}
-
-/* Format a PGP key to a readable hexadecimal string in a user supplied
- * buffer.
- *
- * buffer: the buffer to write into
- * keyid:  the PGP key ID to format
- * len:    the length of buffer, including the null terminator
- *
- * TODO: There is no error checking here.
- * TODO: Make this function more general or use an existing one.
- */
-
-void
-rnp_key_store_format_key(char *buffer, const uint8_t *keyid, int len)
-{
-    unsigned int i;
-    unsigned int n;
-
-    /* Chunks of two bytes are processed at a time because we can
-     * always be reasonably sure that PGP_KEY_ID_SIZE will be
-     * divisible by two. However, if the RFCs specify a fixed
-     * fixed size for PGP key IDs it might be more constructive
-     * to format this in one call and do a compile-time size
-     * check of the constant. If somebody wanted to do
-     * something exotic they can easily re-implement
-     * this function.
-     */
-    for (i = 0, n = 0; i < PGP_KEY_ID_SIZE; i += 2) {
-        n += snprintf(&buffer[n], len - n, "%02x%02x", keyid[i], keyid[i + 1]);
-    }
-    buffer[n] = 0x0;
-}
-
-/* Get the uid of the first key in the keyring.
- *
- * TODO: Set errno on failure.
- * TODO: Check upstream calls to this function - they likely won't
- *       handle the new error condition.
- */
-bool
-rnp_key_store_get_first_ring(rnp_key_store_t *ring, char *id, size_t len, int last)
-{
-    const uint8_t *src;
-
-    /* The NULL test on the ring may not be necessary for non-debug
-     * builds - it would be much better that a NULL ring never
-     * arrived here in the first place.
-     *
-     * The ring length check is a temporary fix for a case where
-     * an empty ring arrives and causes an access violation in
-     * some circumstances.
-     */
-
-    errno = 0;
-
-    if (ring == NULL || rnp_key_store_get_key_count(ring) < 1) {
-        errno = EINVAL;
-        return false;
-    }
-
-    memset(id, 0x0, len);
-
-    list_item *key_item = last ? list_back(rnp_key_store_get_keys(ring)) :
-                                 list_front(rnp_key_store_get_keys(ring));
-    src = pgp_key_get_keyid((pgp_key_t *) key_item);
-    rnp_key_store_format_key(id, src, len);
-
-    return true;
 }
 
 void
