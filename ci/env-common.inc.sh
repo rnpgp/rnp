bda7c572d6a08f4db82991d7656b407193706d13
Daniel Wyatt <daniel.wyatt@ribose.com>
2020-01-27 10:39:54 -0500

    ci: Smarter setting of VERSION_OVERRIDE_BRANCH.

--- ci/env-common.inc.sh
+++ ci/env-common.inc.sh
@@ -6,8 +6,10 @@
 : "${RNP_INSTALL:=$LOCAL_INSTALLS/rnp-install}"
 : "${RUBY_RNP_INSTALL:=$LOCAL_INSTALLS/ruby-rnp}"
 : "${RUBY_RNP_VERSION:=master}"
+: "${VERSION_OVERRIDE_BRANCH:=master}"
 for var in LOCAL_BUILDS LOCAL_INSTALLS BOTAN_INSTALL JSONC_INSTALL \
-  GPG_INSTALL RNP_INSTALL RUBY_RNP_INSTALL RUBY_RNP_VERSION; do
+  GPG_INSTALL RNP_INSTALL RUBY_RNP_INSTALL RUBY_RNP_VERSION \
+  VERSION_OVERRIDE_BRANCH; do
   export "${var?}"
 done
 
@@ -17,5 +19,4 @@ if [ "$BUILD_MODE" = "sanitize" ]; then
 fi
 
 export BOTAN_MODULES=$(cat ci/botan-modules | tr '\n' ',')
-export VERSION_OVERRIDE_BRANCH=master
 
