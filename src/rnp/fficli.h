51288c657fc639f2a7da66bf28286fda8162820d
Nickolay Olshevsky <o.nickolay@gmail.com>
2020-03-03 15:38:41 +0200

    Remove rnp_cfg_t parameter from rnp's rnp_cmd() and underlying functions/tests.

--- src/rnp/fficli.h
+++ src/rnp/fficli.h
@@ -79,13 +79,13 @@ list cli_rnp_get_keylist(cli_rnp_t *rnp, const char *filter, bool secret, bool s
 void cli_rnp_keylist_destroy(list *keys);
 bool cli_rnp_export_keys(cli_rnp_t *rnp, const char *filter);
 bool cli_rnp_export_revocation(cli_rnp_t *rnp, const char *key);
-bool cli_rnp_add_key(const rnp_cfg_t *cfg, cli_rnp_t *rnp);
-bool cli_rnp_dump_file(const rnp_cfg_t *cfg);
-bool cli_rnp_armor_file(const rnp_cfg_t *cfg);
-bool cli_rnp_dearmor_file(const rnp_cfg_t *cfg);
-bool cli_rnp_setup(const rnp_cfg_t *cfg, cli_rnp_t *rnp);
-bool cli_rnp_protect_file(const rnp_cfg_t *cfg, cli_rnp_t *rnp);
-bool cli_rnp_process_file(const rnp_cfg_t *cfg, cli_rnp_t *rnp);
+bool cli_rnp_add_key(cli_rnp_t *rnp);
+bool cli_rnp_dump_file(cli_rnp_t *rnp);
+bool cli_rnp_armor_file(cli_rnp_t *rnp);
+bool cli_rnp_dearmor_file(cli_rnp_t *rnp);
+bool cli_rnp_setup(cli_rnp_t *rnp);
+bool cli_rnp_protect_file(cli_rnp_t *rnp);
+bool cli_rnp_process_file(cli_rnp_t *rnp);
 
 const char *json_obj_get_str(json_object *obj, const char *key);
 int64_t     json_obj_get_int64(json_object *obj, const char *key);
