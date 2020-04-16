3ab72f2bd06e1a60c7206d911958e895d5d28d15
Nickolay Olshevsky <o.nickolay@gmail.com>
2020-04-16 18:22:41 +0300

    Add test for rnp_key_set_expiration() function.

--- src/tests/rnp_tests.h
+++ src/tests/rnp_tests.h
@@ -281,6 +281,8 @@ void test_ffi_rnp_request_password(void **state);
 
 void test_ffi_key_revoke(void **state);
 
+void test_ffi_set_key_expiry(void **state);
+
 void test_dsa_roundtrip(void **state);
 
 void test_dsa_verify_negative(void **state);
