68f4df08541632159c1fc10f87854ee9f1673f8c
Daniel Wyatt <daniel.wyatt@ribose.com>
2020-05-16 09:25:08 -0400

    Fix secp521r1 b & n fields.

--- src/lib/crypto/ec.cpp
+++ src/lib/crypto/ec.cpp
@@ -83,10 +83,10 @@ static const ec_curve_desc_t ec_curves[] = {
    "ffffffffffffffffffffffffffffffffffffffffffff",
    "0x01ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
    "fffffffffffffffffffffffffffffffffffffffffffc",
-   "0x051953eb9618e1c9a1f929a21a0b68540eea2da725b99b315f3b8b489918ef109e156193951ec7e937b1652c"
-   "0bd3bb1bf073573df883d2c34f1ef451fd46b503f00",
-   "0x1fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffa51868783bf2f966b7fcc0"
-   "148f709a5d03bb5c9b8899c47aebb6fb71e91386409",
+   "0x0051953eb9618e1c9a1f929a21a0b68540eea2da725b99b315f3b8b489918ef109e156193951ec7e937b1652"
+   "c0bd3bb1bf073573df883d2c34f1ef451fd46b503f00",
+   "0x01fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffa51868783bf2f966b7fcc"
+   "0148f709a5d03bb5c9b8899c47aebb6fb71e91386409",
    "0x00c6858e06b70404e9cd9e3ecb662395b4429c648139053fb521f828af606b4d3dbaa14b5e77efe75928fe1d"
    "c127a2ffa8de3348b3c1856a429bf97e7e31c2e5bd66",
    "0x011839296a789a3bc0045c8a5fb42c7d1bd998f54449579b446817afbd17273e662c97ee72995ef42640c550"
