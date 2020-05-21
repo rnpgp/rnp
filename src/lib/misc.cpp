f0b2f916631df229e578d8a3aef7787c658b8fc9
Daniel Wyatt <daniel.wyatt@ribose.com>
2020-05-21 05:13:13 -0400

    Address a few static analysis issues. (#1139)

--- src/lib/misc.cpp
+++ src/lib/misc.cpp
@@ -240,8 +240,7 @@ rnp_strcasecmp(const char *s1, const char *s2)
 {
     int n;
 
-    for (n = 0; (n = tolower((uint8_t) *s1) - tolower((uint8_t) *s2)) == 0 && *s1;
-         s1++, s2++) {
+    for (; (n = tolower((uint8_t) *s1) - tolower((uint8_t) *s2)) == 0 && *s1; s1++, s2++) {
     }
     return n;
 }
