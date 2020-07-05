10a8ce32eeba32e627638304f381e1a9d67fd047
Nickolay Olshevsky <o.nickolay@gmail.com>
2020-07-05 15:35:22 +0300

    Allow to process messages wrapped just in a literal packet.

--- src/tests/rnp_tests.h
+++ src/tests/rnp_tests.h
@@ -303,6 +303,8 @@ void test_ffi_key_import_edge_cases(void **state);
 
 void test_ffi_key_remove(void **state);
 
+void test_ffi_literal_packet(void **state);
+
 void test_dsa_roundtrip(void **state);
 
 void test_dsa_verify_negative(void **state);
