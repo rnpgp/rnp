2cbc2debe12ac74e8dc08e3d3ee1d926b8c87343
Tom Ritter <tom@ritter.vg>
2020-07-22 21:03:48 +0000

    Correct copyright years

--- src/fuzzing/keyring.c
+++ src/fuzzing/keyring.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, [Ribose Inc](https://www.ribose.com).
+ * Copyright (c) 2020, [Ribose Inc](https://www.ribose.com).
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
