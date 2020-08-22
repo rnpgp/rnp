08d631b21b3875128323fbef7cad75007b7d1949
Nickolay Olshevsky <o.nickolay@gmail.com>
2020-08-22 11:49:41 +0300

    Update tests with 32k-boundary CRLF edge case.

--- src/tests/ffi.cpp
+++ src/tests/ffi.cpp
@@ -7389,6 +7389,36 @@ TEST_F(rnp_tests, test_ffi_op_verify_sig_count)
     rnp_input_destroy(source);
     rnp_input_destroy(input);
 
+    /* detached text-mode signature with CRLF on 32k boundary */
+    source = NULL;
+    sigcount = 255;
+    assert_rnp_success(
+      rnp_input_from_path(&source, "data/test_messages/message-32k-crlf.txt"));
+    assert_rnp_success(
+      rnp_input_from_path(&input, "data/test_messages/message-32k-crlf.txt.sig"));
+    assert_rnp_success(rnp_op_verify_detached_create(&verify, ffi, source, input));
+    assert_rnp_success(rnp_op_verify_execute(verify));
+    assert_rnp_success(rnp_op_verify_get_signature_count(verify, &sigcount));
+    assert_int_equal(sigcount, 1);
+    assert_true(check_signature(verify, 0, RNP_SUCCESS));
+    rnp_op_verify_destroy(verify);
+    rnp_input_destroy(source);
+    rnp_input_destroy(input);
+
+    /* embedded text-mode signature with CRLF on 32k boundary */
+    sigcount = 255;
+    assert_rnp_success(
+      rnp_input_from_path(&input, "data/test_messages/message-32k-crlf.txt.gpg"));
+    assert_rnp_success(rnp_output_to_null(&output));
+    assert_rnp_success(rnp_op_verify_create(&verify, ffi, input, output));
+    assert_rnp_success(rnp_op_verify_execute(verify));
+    assert_rnp_success(rnp_op_verify_get_signature_count(verify, &sigcount));
+    assert_int_equal(sigcount, 1);
+    assert_true(check_signature(verify, 0, RNP_SUCCESS));
+    rnp_op_verify_destroy(verify);
+    rnp_input_destroy(input);
+    rnp_output_destroy(output);
+
     /* malformed detached signature */
     sigcount = 255;
     assert_rnp_success(rnp_input_from_path(&source, "data/test_messages/message.txt"));
