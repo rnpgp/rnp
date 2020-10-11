7519a5d439b7b9bfcbcb78bfd33e01535017c80f
Roman Shevchenko <rrrooommmaaa@mail.ru>
2020-10-11 22:20:42 +0300

    rnp_mkstemp() implementation

--- src/common/file-utils.cpp
+++ src/common/file-utils.cpp
@@ -34,6 +34,9 @@
 #include <stdio.h>
 #include "uniwin.h"
 #include <errno.h>
+#include <locale>
+#include <codecvt>
+#include <random>
 #ifdef HAVE_FCNTL_H
 #include <fcntl.h>
 #endif
@@ -62,107 +65,59 @@ rnp_filemtime(const char *path)
 }
 
 #ifdef _MSC_VER
-/* mkstemp extracted from libc/sysdeps/posix/tempname.c.  Copyright
-   (C) 1991-1999, 2000, 2001, 2006 Free Software Foundation, Inc.
+static const char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.  */
-
-static const char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-/* Generate a temporary file name based on TMPL.  TMPL must match the
-   rules for mk[s]temp (i.e. end in "XXXXXX").  The name constructed
-   does not exist at the time of the call to mkstemp.  TMPL is
-   overwritten with the result.  */
+/** @private
+ *  generate a temporary file name based on TMPL.
+ *
+ *  @param tmpl filename template in UTF-8 ending in XXXXXX
+ *  @return file descriptor of newly created and opened file, or -1 on error
+ **/
 int
 rnp_mkstemp(char *tmpl)
 {
-    size_t                    len;
-    char *                    XXXXXX;
-    static unsigned long long value;
-    unsigned long long        random_time_bits;
-    unsigned int              count;
-    int                       fd = -1;
-    int                       save_errno = errno;
-
-    /* A lower bound on the number of temporary files to attempt to
-       generate.  The maximum total number of temporary file names that
-       can exist for a given template is 62**6.  It should never be
-       necessary to try all these combinations.  Instead if a reasonable
-       number of names is tried (we define reasonable as 62**3) fail to
-       give the system administrator the chance to remove the problems.  */
-#define ATTEMPTS_MIN (62 * 62 * 62)
-
-    /* The number of times to attempt to generate a temporary file.  To
-       conform to POSIX, this must be no smaller than TMP_MAX.  */
-#if ATTEMPTS_MIN < TMP_MAX
-    unsigned int attempts = TMP_MAX;
-#else
-    unsigned int attempts = ATTEMPTS_MIN;
-#endif
-
-    len = strlen(tmpl);
-    if (len < 6 || strcmp(&tmpl[len - 6], "XXXXXX")) {
+    int       save_errno = errno;
+    const int mask_length = 6;
+    int       len = strlen(tmpl);
+    if (len < mask_length || strcmp(&tmpl[len - mask_length], "XXXXXX")) {
         errno = EINVAL;
         return -1;
     }
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> utf8conv;
+    std::wstring tmpl_w = utf8conv.from_bytes(tmpl, tmpl + len - mask_length);
 
     /* This is where the Xs start.  */
-    XXXXXX = &tmpl[len - 6];
+    char *XXXXXX = &tmpl[len - mask_length];
 
-    /* Get some more or less random data.  */
-    {
-        SYSTEMTIME stNow;
-        FILETIME   ftNow;
+    std::random_device rd;
+    std::mt19937_64    rng(rd());
 
-        // get system time
-        GetSystemTime(&stNow);
-        stNow.wMilliseconds = 500;
-        if (!SystemTimeToFileTime(&stNow, &ftNow)) {
-            errno = -1;
-            return -1;
-        }
+    for (unsigned int countdown = TMP_MAX; --countdown;) {
+        unsigned long long v = rng();
 
-        random_time_bits = (((unsigned long long) ftNow.dwHighDateTime << 32) |
-                            (unsigned long long) ftNow.dwLowDateTime);
-    }
-    value += random_time_bits ^ (unsigned long long) GetCurrentThreadId();
+        XXXXXX[0] = letters[v % 36];
+        v /= 36;
+        XXXXXX[1] = letters[v % 36];
+        v /= 36;
+        XXXXXX[2] = letters[v % 36];
+        v /= 36;
+        XXXXXX[3] = letters[v % 36];
+        v /= 36;
+        XXXXXX[4] = letters[v % 36];
+        v /= 36;
+        XXXXXX[5] = letters[v % 36];
 
-    for (count = 0; count < attempts; value += 7777, ++count) {
-        unsigned long long v = value;
-
-        /* Fill in the random bits.  */
-        XXXXXX[0] = letters[v % 62];
-        v /= 62;
-        XXXXXX[1] = letters[v % 62];
-        v /= 62;
-        XXXXXX[2] = letters[v % 62];
-        v /= 62;
-        XXXXXX[3] = letters[v % 62];
-        v /= 62;
-        XXXXXX[4] = letters[v % 62];
-        v /= 62;
-        XXXXXX[5] = letters[v % 62];
-
-        int flags = O_WRONLY | O_CREAT | O_EXCL;
-#ifdef HAVE_O_BINARY
-        flags |= O_BINARY;
-#else
-#ifdef HAVE__O_BINARY
-        flags |= _O_BINARY;
-#endif
-#endif
-        fd = open(tmpl, flags, _S_IREAD | _S_IWRITE);
-        if (fd >= 0) {
+        int flags = O_WRONLY | O_CREAT | O_EXCL | O_BINARY;
+        int fd =
+          _wopen((tmpl_w + utf8conv.from_bytes(XXXXXX)).c_str(), flags, _S_IREAD | _S_IWRITE);
+        if (fd != -1) {
             errno = save_errno;
             return fd;
         } else if (errno != EEXIST)
             return -1;
     }
 
-    /* We got out of the loop because we ran out of combinations to try.  */
+    // We got out of the loop because we ran out of combinations to try.
     errno = EEXIST;
     return -1;
 }
