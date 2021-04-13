604dd0a739d17856c16d1b4241e6c184572bd1fe
Nickolay Olshevsky <o.nickolay@gmail.com>
2021-04-13 13:21:49 +0300

    Extract ec_generate_pkey to the separate function, so it may be reused for ECDH.

--- src/lib/crypto/ec_ossl.cpp
+++ src/lib/crypto/ec_ossl.cpp
@@ -40,33 +40,27 @@ x25519_generate(rng_t *rng, pgp_ec_key_t *key)
     return RNP_ERROR_NOT_IMPLEMENTED;
 }
 
-rnp_result_t
-ec_generate(rng_t *                rng,
-            pgp_ec_key_t *         key,
-            const pgp_pubkey_alg_t alg_id,
-            const pgp_curve_t      curve)
+EVP_PKEY *
+ec_generate_pkey(const pgp_pubkey_alg_t alg_id, const pgp_curve_t curve)
 {
     if (!alg_allows_curve(alg_id, curve)) {
-        return RNP_ERROR_BAD_PARAMETERS;
+        return NULL;
     }
-
     const ec_curve_desc_t *ec_desc = get_curve_desc(curve);
     if (!ec_desc) {
-        return RNP_ERROR_BAD_PARAMETERS;
+        return NULL;
     }
     int nid = OBJ_sn2nid(ec_desc->openssl_name);
     if (nid == NID_undef) {
         RNP_LOG("Unknown SN: %s", ec_desc->openssl_name);
-        return RNP_ERROR_BAD_PARAMETERS;
+        return NULL;
     }
     EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
     if (!ctx) {
         RNP_LOG("Failed to create ctx: %lu", ERR_peek_last_error());
-        return RNP_ERROR_GENERIC;
+        return NULL;
     }
-    rnp_result_t ret = RNP_ERROR_GENERIC;
-    EVP_PKEY *   pkey = NULL;
-    EC_KEY *     ec = NULL;
+    EVP_PKEY *pkey = NULL;
     if (EVP_PKEY_keygen_init(ctx) <= 0) {
         RNP_LOG("Failed to init keygen: %lu", ERR_peek_last_error());
         goto done;
@@ -77,9 +71,25 @@ ec_generate(rng_t *                rng,
     }
     if (EVP_PKEY_keygen(ctx, &pkey) <= 0) {
         RNP_LOG("EC keygen failed: %lu", ERR_peek_last_error());
-        goto done;
     }
-    ec = EVP_PKEY_get0_EC_KEY(pkey);
+done:
+    EVP_PKEY_CTX_free(ctx);
+    return pkey;
+}
+
+rnp_result_t
+ec_generate(rng_t *                rng,
+            pgp_ec_key_t *         key,
+            const pgp_pubkey_alg_t alg_id,
+            const pgp_curve_t      curve)
+{
+    EVP_PKEY *pkey = ec_generate_pkey(alg_id, curve);
+    if (!pkey) {
+        return RNP_ERROR_BAD_PARAMETERS;
+    }
+
+    rnp_result_t ret = RNP_ERROR_GENERIC;
+    EC_KEY *     ec = EVP_PKEY_get0_EC_KEY(pkey);
     if (!ec) {
         RNP_LOG("Failed to retrieve EC key: %lu", ERR_peek_last_error());
         goto done;
@@ -108,7 +118,6 @@ ec_generate(rng_t *                rng,
     }
 done:
     EVP_PKEY_free(pkey);
-    EVP_PKEY_CTX_free(ctx);
     return ret;
 }
 
