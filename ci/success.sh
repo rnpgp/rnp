3ab2d87e6671b085133e508ee99008135375a328
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-04-23 11:31:09 +0800

    ci: Extract common install functionality to separate files

--- ci/success.sh
+++ ci/success.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -eu
 
+: "${BUILD_MODE:=normal}"
+
 if [ "$BUILD_MODE" = "coverage" ]; then
   bash <(curl -s https://codecov.io/bash) \
     -K \
     -p "${LOCAL_BUILDS}/rnp-build" \
     -R "$GITHUB_WORKSPACE"
 fi
-
