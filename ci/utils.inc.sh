5152e8adfc88f0022f6ddef4cd89706b0de9b8fa
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-04-23 11:31:09 +0800

    ci: Extract common install functionality to separate files

--- ci/utils.inc.sh
+++ ci/utils.inc.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
 # Derived from: https://gist.github.com/marcusandre/4b88c2428220ea255b83
 get_os() {
-  local ostype=$(echo $OSTYPE | tr '[:upper:]' '[:lower:]')
+  local ostype
+  ostype=$(<<< "$OSTYPE" tr '[:upper:]' '[:lower:]')
   if [ -z "$ostype" ]; then
     ostype=$(uname | tr '[:upper:]' '[:lower:]')
   fi
@@ -19,44 +21,23 @@ get_os() {
 }
 
 get_linux_dist() {
-  if [ -f /etc/os-release ]; then
+  if [[ -f /etc/os-release ]]; then
     sh -c '. /etc/os-release && echo $ID'
   elif type lsb_release >/dev/null 2>&1; then
     lsb_release -si | tr '[:upper:]' '[:lower:]'
   fi
 }
 
-build_and_install_automake() {
-  # automake
-  automake_build=${LOCAL_BUILDS}/automake
-  mkdir -p "${automake_build}"
-  pushd ${automake_build}
-  curl -L -o automake.tar.xz https://ftp.gnu.org/gnu/automake/automake-1.16.1.tar.xz
-  tar -xf automake.tar.xz --strip 1
-  ./configure --enable-optimizations --prefix=/usr && ${MAKE} -j${MAKE_PARALLEL} && ${SUDO} make install
-  popd
-}
+# If target does not exist, create symlink from source to target.
+ensure_symlink_to_target() {
+  local from="${1:?Missing source}"
+  local to="${2:?Missing target}"
 
-build_and_install_cmake() {
-  # cmake
-  cmake_build=${LOCAL_BUILDS}/cmake
-  mkdir -p "${cmake_build}"
-  pushd ${cmake_build}
-  wget https://github.com/Kitware/CMake/releases/download/v3.19.6/cmake-3.19.6.tar.gz -O cmake.tar.gz
-  tar xzf cmake.tar.gz --strip 1
-  ./configure --prefix=/usr && ${MAKE} -j${MAKE_PARALLEL} && ${SUDO} make install
-  popd
-  CMAKE=/usr/bin/cmake
-}
-
-build_and_install_python() {
-  # python
-  python_build=${LOCAL_BUILDS}/python
-  mkdir -p "${python_build}"
-  pushd ${python_build}
-  curl -L -o python.tar.xz https://www.python.org/ftp/python/3.9.2/Python-3.9.2.tar.xz
-  tar -xf python.tar.xz --strip 1
-  ./configure --enable-optimizations --prefix=/usr && ${MAKE} -j${MAKE_PARALLEL} && ${SUDO} make install
-  ${SUDO} ln -sf /usr/bin/python3 /usr/bin/python
-  popd
+  if [[ -e "${from}" && ! -e "${to}" ]]; then
+    if ! sudo ln -s "${from}" "${to}"
+    then
+      >&2 echo "Error: ${to} still not available after symlink.  Aborting."
+      exit 1
+    fi
+  fi
 }
