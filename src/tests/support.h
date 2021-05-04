816ebff54ea25e999914787ae65232bb6c6ee7f8
Nickolay Olshevsky <o.nickolay@gmail.com>
2021-05-04 12:53:23 +0300

    Make use of ENABLE_SM2 in FFI and add test which checks rnp_supports_feature()/rnp_supported_features().

--- src/tests/support.h
+++ src/tests/support.h
@@ -239,4 +239,6 @@ bool import_all_keys(rnp_ffi_t ffi, const uint8_t *data, size_t len);
 bool import_pub_keys(rnp_ffi_t ffi, const uint8_t *data, size_t len);
 bool import_sec_keys(rnp_ffi_t ffi, const uint8_t *data, size_t len);
 
+bool sm2_enabled();
+
 #endif /* SUPPORT_H_ */
