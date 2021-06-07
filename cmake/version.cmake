5e714fc88aa64157f8150c1b81aa4d11b71874c7
Nickolay Olshevsky <o.nickolay@gmail.com>
2021-06-07 17:19:23 +0300

    Add version.txt to the repository and update CMake version detection code.

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 Ribose Inc.
+# Copyright (c) 2018-2021 Ribose Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -52,7 +52,7 @@ function(extract_version_info version var_prefix)
   #   v1.9.0-3-g5b92266-dirty+1546836556
   string(REGEX MATCH "^v?([0-9]+\\.[0-9]+\\.[0-9]+)(-([0-9]+)-g([0-9a-f]+)(-dirty)?)?(\\+([0-9]+))?$" matches "${version}")
   if (NOT matches)
-    message(FATAL_ERROR "Failed to extract version components.")
+    message(FATAL_ERROR "Failed to extract version components from ${version}.")
   endif()
   set(${var_prefix}_VERSION "${CMAKE_MATCH_1}" PARENT_SCOPE) # 1.9.0
   if (NOT CMAKE_MATCH_3)
@@ -76,22 +76,34 @@ function(extract_version_info version var_prefix)
 endfunction()
 
 function(determine_version source_dir var_prefix)
+  set(has_release_tag NO)
+  set(has_version_txt NO)
+  set(local_prefix "_determine_ver")
+  # find out base version via version.txt
+  set(base_version "v0.0.0")
   if (EXISTS "${source_dir}/version.txt")
-    # same as below, but used for snapshots
-    file(STRINGS "${source_dir}/version.txt" version)
+    set(has_version_txt YES)
+    file(STRINGS "${source_dir}/version.txt" version_file)
+    extract_version_info("${version_file}" "${local_prefix}")
+    set(base_version "v${${local_prefix}_VERSION}")
+    message(STATUS "Found version.txt with ${version_file}")
   else()
-    # for GIT_EXECUTABLE
-    find_package(Git REQUIRED)
-    # get a description of the version, something like:
-    #   v1.9.1-0-g38ffe82        (a tagged release)
-    #   v1.9.1-0-g38ffe82-dirty  (a tagged release with local modifications)
-    #   v1.9.0-3-g5b92266        (post-release snapshot)
-    #   v1.9.0-3-g5b92266-dirty  (post-release snapshot with local modifications)
-    _git(version describe --abbrev=${GIT_REV_LEN} --match "v[0-9]*" --long --dirty)
-    if (NOT _git_ec EQUAL 0)
-      # no annotated tags, fake one
-      git(revision rev-parse --short=${GIT_REV_LEN} --verify HEAD)
-      set(version "v0.0.0-0-g${revision}")
+    message(STATUS "Found no version.txt.")
+  endif()
+  # for GIT_EXECUTABLE
+  find_package(Git REQUIRED)
+  # get a description of the version, something like:
+  #   v1.9.1-0-g38ffe82        (a tagged release)
+  #   v1.9.1-0-g38ffe82-dirty  (a tagged release with local modifications)
+  #   v1.9.0-3-g5b92266        (post-release snapshot)
+  #   v1.9.0-3-g5b92266-dirty  (post-release snapshot with local modifications)
+  _git(version describe --abbrev=${GIT_REV_LEN} --match "v[0-9]*" --long --dirty)
+  if (NOT _git_ec EQUAL 0)
+    # no annotated tags, fake one
+    message(STATUS "Found no annotated tags.")
+    _git(revision rev-parse --short=${GIT_REV_LEN} --verify HEAD)
+    if (_git_ec EQUAL 0)
+      set(version "${base_version}-0-g${revision}")
       # check if dirty (this won't detect untracked files, but should be ok)
       _git(changes diff-index --quiet HEAD --)
       if (NOT _git_ec EQUAL 0)
@@ -99,11 +111,27 @@ function(determine_version source_dir var_prefix)
       endif()
       # append the commit timestamp of the most recent commit (only
       # in non-release branches -- typically master)
-      git(commit_timestamp show -s --format=%ct)
-      string(APPEND version "+${commit_timestamp}")
+      _git(commit_timestamp show -s --format=%ct)
+      if (_git_ec EQUAL 0)
+        string(APPEND version "+${commit_timestamp}")
+      endif()
+    elseif(has_version_txt)
+      # Nothing to get from git - so use version.txt completely
+      set(version "${version_file}")
+    else()
+      # Sad case - no git, no version.txt
+      set(version "${base_version}")
+    endif()
+  else()
+    set(has_release_tag YES)
+    message(STATUS "Found annotated tag ${version}")
+    if("${has_version_txt}")
+      string(FIND "${version}" "${base_version}" _base_version_pos)
+      if("${has_version_txt}" AND NOT "${_base_version_pos}" EQUAL 0)
+        message(WARNING "Tagged version ${version} doesn't match one from the version.txt: ${base_version}")
+      endif()
     endif()
   endif()
-  set(local_prefix "_determine_ver")
   extract_version_info("${version}" "${local_prefix}")
   foreach(suffix VERSION VERSION_NCOMMITS VERSION_GIT_REV VERSION_IS_DIRTY VERSION_COMMIT_TIMESTAMP)
     if (NOT DEFINED ${local_prefix}_${suffix})
@@ -114,26 +142,24 @@ function(determine_version source_dir var_prefix)
   endforeach()
   # Set VERSION_SUFFIX and VERSION_FULL. When making changes, be aware that
   # this is used in packaging as well and will affect ordering.
-  # | state            | version_full                |
-  # |------------------------------------------------|
-  # | exact tag        | 0.9.0                       |
-  # | exact tag, dirty | 0.9.0+git20180604           |
-  # | after tag        | 0.9.0+git20180604.1.085039f |
-  # | no tag           | 0.0.0+git20180604.2ee02af   |
+  # | state                 | version_full                |
+  # |-----------------------------------------------------|
+  # | exact tag             | 0.9.0                       |
+  # | exact tag, dirty      | 0.9.0+git20180604           |
+  # | after tag             | 0.9.0+git20180604.1.085039f |
+  # | no tag, version.txt   | 0.9.0+git20180604.2ee02af   |
+  # | no tag, no version.txt| 0.0.0+git20180604.2ee02af   |
   string(TIMESTAMP date "%Y%m%d" UTC)
   set(version_suffix "")
-  if ((NOT ${local_prefix}_VERSION_NCOMMITS EQUAL 0) OR (${local_prefix}_VERSION STREQUAL "0.0.0"))
+  if (NOT ${local_prefix}_VERSION_NCOMMITS EQUAL 0)
     # 0.9.0+git20150604.4.289818b
+    string(APPEND version_suffix "+git${date}.${${local_prefix}_VERSION_NCOMMITS}.${${local_prefix}_VERSION_GIT_REV}")
+  elseif ((NOT has_release_tag) AND ((NOT has_version_txt) OR ("${base_version}" STREQUAL "v0.0.0") OR (NOT "${revision}" STREQUAL "")))
+    # 0.9.0+git20150604.289818b
+    string(APPEND version_suffix "+git${date}.${${local_prefix}_VERSION_GIT_REV}")
+  elseif(${local_prefix}_VERSION_IS_DIRTY)
+    # 0.9.0+git20150604
     string(APPEND version_suffix "+git${date}")
-    if (NOT ${local_prefix}_VERSION_NCOMMITS EQUAL 0)
-      string(APPEND version_suffix ".${${local_prefix}_VERSION_NCOMMITS}")
-    endif()
-    string(APPEND version_suffix ".${${local_prefix}_VERSION_GIT_REV}")
-  else()
-    if (${local_prefix}_VERSION_IS_DIRTY)
-      # 0.9.0+git20150604
-      string(APPEND version_suffix "+git${date}")
-    endif()
   endif()
   set(version_full "${${local_prefix}_VERSION}${version_suffix}")
   # set the results
