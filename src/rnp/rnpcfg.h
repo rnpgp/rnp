2cdc639311a30f12675fcaaace2920678c1d7bcb
Nickolay Olshevsky <o.nickolay@gmail.com>
2021-09-24 14:20:51 +0300

    Add command rnpkeys --edit-key to check and fix Curve25519 bits to the CLI.

--- src/rnp/rnpcfg.h
+++ src/rnp/rnpcfg.h
@@ -87,6 +87,8 @@
 #define CFG_REV_REASON "rev-reason" /* revocation reason human-readable string */
 #define CFG_PERMISSIVE "permissive" /* ignore bad packets during key import */
 #define CFG_NOTTY "notty" /* disable tty usage and do input/output via stdin/stdout */
+#define CFG_FIX_25519_BITS "fix-25519-bits"   /* fix Cv25519 secret key via --edit-key */
+#define CFG_CHK_25519_BITS "check-25519-bits" /* check Cv25519 secret key bits */
 
 /* rnp keyring setup variables */
 #define CFG_KR_PUB_FORMAT "kr-pub-format"
