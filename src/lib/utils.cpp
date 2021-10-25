79054ae6d8609725262c8acf07dc83a5b50370db
Nickolay Olshevsky <o.nickolay@gmail.com>
2021-10-25 14:27:29 +0300

    Move code from misc.cpp to utils.cpp and remove misc.cpp.

--- src/lib/utils.cpp
+++ src/lib/utils.cpp
@@ -1,11 +1,6 @@
 /*
- * Copyright (c) 2017-2020 [Ribose Inc](https://www.ribose.com).
- * Copyright (c) 2009-2010 The NetBSD Foundation, Inc.
+ * Copyright (c) 2021, [Ribose Inc](https://www.ribose.com).
  * All rights reserved.
- *
- * This code is originally derived from software contributed to
- * The NetBSD Foundation by Alistair Crooks (agc@netbsd.org), and
- * carried further by Ribose Inc (https://www.ribose.com).
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,70 +23,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-/*
- * Copyright (c) 2005-2008 Nominet UK (www.nic.uk)
- * All rights reserved.
- * Contributors: Ben Laurie, Rachel Willmer. The Contributors have asserted
- * their moral rights under the UK Copyright Design and Patents Act 1988 to
- * be recorded as the authors of this copyright work.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.
- *
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-/** \file
- */
-#include "config.h"
-
-#ifdef HAVE_SYS_CDEFS_H
-#include <sys/cdefs.h>
-#endif
-
-#if defined(__NetBSD__)
-__COPYRIGHT("@(#) Copyright (c) 2009 The NetBSD Foundation, Inc. All rights reserved.");
-__RCSID("$NetBSD: misc.c,v 1.41 2012/03/05 02:20:18 christos Exp $");
-#endif
-
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#include <ctype.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <errno.h>
-
-#ifdef HAVE_UNISTD_H
-#include <sys/param.h>
-#include <unistd.h>
-#else
-#include "uniwin.h"
-#endif
-
-#include "crypto.h"
-#include "crypto/mem.h"
-#include "utils.h"
+#include "types.h"
 #include "str-utils.h"
-#include "json-utils.h"
-
-#ifdef _WIN32
-#include <windows.h>
-#include <locale>
-#include <codecvt>
-#define vsnprintf _vsnprintf
-#endif
 
 const char *
 id_str_pair::lookup(const id_str_pair pair[], int id, const char *notfound)
