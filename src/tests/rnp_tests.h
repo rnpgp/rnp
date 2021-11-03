5f44e098fe09979743a17381f9da0f8e83ebc348
Nickolay Olshevsky <o.nickolay@gmail.com>
2021-11-03 15:39:48 +0200

    Use rnp::Hash instead of pgp_hash_t where applicable.

--- src/tests/rnp_tests.h
+++ src/tests/rnp_tests.h
@@ -469,5 +469,6 @@ void test_fuzz_verify_detached(void **state);
 #define assert_rnp_failure(a) EXPECT_NE((a), RNP_SUCCESS)
 #define assert_memory_equal(a, b, sz) EXPECT_EQ(0, memcmp((a), (b), (sz)))
 #define assert_memory_not_equal(a, b, sz) EXPECT_NE(0, memcmp((a), (b), (sz)))
+#define assert_throw(a) EXPECT_ANY_THROW((a))
 
 #endif // RNP_TESTS_H
