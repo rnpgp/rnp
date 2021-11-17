2e0570cdcea599f8ce1b19deda1696b3655f2b91
Nickolay Olshevsky <o.nickolay@gmail.com>
2021-11-17 13:46:15 +0200

    Fix codecov uploads and use new non-bash uploader.

--- ci/success.sh
+++ ci/success.sh
@@ -4,8 +4,13 @@ set -eu
 : "${BUILD_MODE:=normal}"
 
 if [ "$BUILD_MODE" = "coverage" ]; then
-  bash <(curl -s https://codecov.io/bash) \
-    -K \
-    -p "${LOCAL_BUILDS}/rnp-build" \
-    -R "$GITHUB_WORKSPACE"
+    curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import # One-time step
+    curl -Os https://uploader.codecov.io/latest/linux/codecov
+    curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
+    curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+    gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
+    shasum -a 256 -c codecov.SHA256SUM
+    chmod +x codecov
+    find "${LOCAL_BUILDS}/rnp-build/" -type f -name '*.gcno' -exec gcov {} +
+    ./codecov
 fi
