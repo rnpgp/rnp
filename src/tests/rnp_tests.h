4bae9ea7dada71e95e5e03e1e4583ea0c69a0176
Anton <anton@corp.bluecherry.net>
2022-01-04 01:44:36 +0400

    Add simple repetition count test for RNG.

--- src/tests/rnp_tests.h
+++ src/tests/rnp_tests.h
@@ -457,6 +457,8 @@ void test_fuzz_dump(void **state);
 
 void test_fuzz_verify_detached(void **state);
 
+void test_rng_randomness(void **state);
+
 #define assert_true(a) EXPECT_TRUE((a))
 #define assert_false(a) EXPECT_FALSE((a))
 #define assert_string_equal(a, b) EXPECT_STREQ((a), (b))
