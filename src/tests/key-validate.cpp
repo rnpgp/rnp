4397d4f141323b2e216a61f1a53a267e7c09f536
Nickolay Olshevsky <o.nickolay@gmail.com>
2022-03-24 14:22:58 +0200

    Refactor function process_pgp_key() to use armored stream wrapper.

--- src/tests/key-validate.cpp
+++ src/tests/key-validate.cpp
@@ -158,7 +158,7 @@ key_store_add(rnp_key_store_t *keyring, const char *keypath)
     pgp_transferable_key_t tkey = {};
 
     assert_rnp_success(init_file_src(&keysrc, keypath));
-    assert_rnp_success(process_pgp_key(&keysrc, tkey, false));
+    assert_rnp_success(process_pgp_key(keysrc, tkey, false));
     assert_true(rnp_key_store_add_transferable_key(keyring, &tkey));
     src_close(&keysrc);
 }
