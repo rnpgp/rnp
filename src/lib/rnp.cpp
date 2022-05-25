d76a2d7c898c567b2df97c4bed411691c981c2bd
Nickolay Olshevsky <o.nickolay@gmail.com>
2022-05-25 15:09:29 +0300

    Add missing FFI_GUARD for the rnp_request_password.

--- src/lib/rnp.cpp
+++ src/lib/rnp.cpp
@@ -1262,7 +1262,7 @@ FFI_GUARD
 
 rnp_result_t
 rnp_request_password(rnp_ffi_t ffi, rnp_key_handle_t key, const char *context, char **password)
-{
+try {
     if (!ffi || !password || !ffi->getpasscb) {
         return RNP_ERROR_NULL_POINTER;
     }
@@ -1281,6 +1281,7 @@ rnp_request_password(rnp_ffi_t ffi, rnp_key_handle_t key, const char *context, c
     memcpy(*password, pass.data(), pass_len);
     return RNP_SUCCESS;
 }
+FFI_GUARD
 
 static rnp_result_t
 load_keys_from_input(rnp_ffi_t ffi, rnp_input_t input, rnp_key_store_t *store)
