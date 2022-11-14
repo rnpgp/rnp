a25cd52cdf4274f82a5d5525e6a393a8db979821
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-11-14 22:45:45 +0300

    SEXP library integration

--- src/librekey/g10_sexp.hpp
+++ src/librekey/g10_sexp.hpp
@@ -27,58 +27,35 @@
 #ifndef RNP_G10_SEXP_HPP
 #define RNP_G10_SEXP_HPP
 
+
+#include "sexp/sexp.h"
+
 #define SXP_MAX_DEPTH 30
 
-class s_exp_element_t {
-  protected:
-    bool is_block_;
+typedef sexp::sexp_object_t s_exp_element_t;
 
+class s_exp_block_t : public sexp::sexp_string_t {
   public:
-    s_exp_element_t(bool block) : is_block_(block){};
-    virtual ~s_exp_element_t() = default;
-    virtual bool
-    write(pgp_dest_t &dst) const noexcept
-    {
-        return false;
-    };
-
-    bool
-    is_block()
-    {
-        return is_block_;
-    }
-};
-
-class s_exp_block_t : public s_exp_element_t {
-  protected:
-    std::vector<uint8_t> bytes_;
-
-  public:
-    s_exp_block_t(const uint8_t *bt = NULL, size_t ln = 0)
-        : s_exp_element_t(true),
-          bytes_(bt ? std::vector<uint8_t>(bt, bt + ln) : std::vector<uint8_t>()){};
+    s_exp_block_t(const uint8_t *bt, size_t ln) {
+      data_string = sexp::sexp_simple_string_t(bt, ln);
+     }
+    s_exp_block_t(void) : sexp::sexp_string_t() { }
+    s_exp_block_t(const uint8_t *bt) : sexp::sexp_string_t(bt) { }
     s_exp_block_t(unsigned u);
-    s_exp_block_t(const std::string &str)
-        : s_exp_element_t(true),
-          bytes_((uint8_t *) str.data(), (uint8_t *) (str.data() + str.size())){};
+    s_exp_block_t(const std::string &str): sexp::sexp_string_t(str) { }
     s_exp_block_t(const pgp_mpi_t &mpi);
-    unsigned as_unsigned() const noexcept;
-    bool     write(pgp_dest_t &dst) const noexcept;
 
-    const std::vector<uint8_t> &
-    bytes() const
-    {
-        return bytes_;
-    }
+//    static bool  write(const s_exp_block_t &s_exp, pgp_dest_t &dst) noexcept;
+
+
 };
 
-class s_exp_t : public s_exp_element_t {
-    std::vector<std::unique_ptr<s_exp_element_t>> elements_;
+class s_exp_t : public sexp::sexp_list_t {
     /* write s_exp_t contents, adding padding, for the further encryption */
     rnp::secure_vector<uint8_t> write_padded(size_t padblock) const;
 
   public:
-    s_exp_t() : s_exp_element_t(false){};
+
     bool     parse(const char **r_bytes, size_t *r_length, size_t depth = 1);
     void     add(std::unique_ptr<s_exp_element_t> sptr);
     void     add(const std::string &str);
@@ -87,23 +64,8 @@ class s_exp_t : public s_exp_element_t {
     s_exp_t &add_sub();
     bool     write(pgp_dest_t &dst) const noexcept;
 
-    size_t
-    size() const
-    {
-        return elements_.size();
-    }
+//    static bool  write(const s_exp_t &s_exp, pgp_dest_t &dst) noexcept;
 
-    s_exp_element_t &
-    at(size_t idx)
-    {
-        return *elements_[idx].get();
-    }
-
-    s_exp_t *      lookup_var(const std::string &name) noexcept;
-    s_exp_block_t *lookup_var_data(const std::string &name) noexcept;
-
-    bool read_mpi(const std::string &name, pgp_mpi_t &val) noexcept;
-    bool read_curve(const std::string &name, pgp_ec_key_t &key) noexcept;
     void add_mpi(const std::string &name, const pgp_mpi_t &val);
     void add_curve(const std::string &name, const pgp_ec_key_t &key);
     void add_pubkey(const pgp_key_pkt_t &key);
@@ -112,7 +74,6 @@ class s_exp_t : public s_exp_element_t {
                               const std::string &   password,
                               rnp::SecurityContext &ctx);
 
-    void clear();
 };
 
 #endif
