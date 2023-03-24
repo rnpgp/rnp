daef2d162405c7e0516276a64ccc2f7fd63affa1
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2023-03-24 21:40:09 +0300

    Fixed nix workflow

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, sexp }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
