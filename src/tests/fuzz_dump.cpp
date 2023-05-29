bd03a51e91471eec61e67fd18b342e0d706a149f
Nickolay Olshevsky <o.nickolay@gmail.com>
2023-05-29 14:53:11 +0300

    Add test case for oss-fuzz issue 58681.

--- src/tests/fuzz_dump.cpp
+++ src/tests/fuzz_dump.cpp
@@ -55,4 +55,7 @@ TEST_F(rnp_tests, test_fuzz_dump)
 
     data = file_to_vec(DATA_PATH "timeout-6057122298462208");
     assert_int_equal(dump_LLVMFuzzerTestOneInput(data.data(), data.size()), 0);
+
+    data = file_to_vec(DATA_PATH "outofmemory-6111789935624192");
+    assert_int_equal(dump_LLVMFuzzerTestOneInput(data.data(), data.size()), 0);
 }
