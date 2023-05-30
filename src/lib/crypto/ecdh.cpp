5323c06228405114a6e3d964e46655f18fe4156d
Nickolay Olshevsky <o.nickolay@gmail.com>
2023-05-30 13:35:44 +0300

    Fix Botan3 deprecations.

--- src/lib/crypto/ecdh.cpp
+++ src/lib/crypto/ecdh.cpp
@@ -261,7 +261,13 @@ ecdh_encrypt_pkcs5(rnp::RNG *               rng,
     }
 
     out->mlen = sizeof(out->m);
+#if defined(CRYPTO_BACKEND_BOTAN3)
+    char name[8];
+    snprintf(name, sizeof(name), "AES-%zu", 8 * kek_len);
+    if (botan_nist_kw_enc(name, 0, m, m_padded_len, kek, kek_len, out->m, &out->mlen)) {
+#else
     if (botan_key_wrap3394(m, m_padded_len, kek, kek_len, out->m, &out->mlen)) {
+#endif
         goto end;
     }
 
@@ -354,8 +360,15 @@ ecdh_decrypt_pkcs5(uint8_t *                   out,
         goto end;
     }
 
+#if defined(CRYPTO_BACKEND_BOTAN3)
+    char name[8];
+    snprintf(name, sizeof(name), "AES-%zu", 8 * kek_len);
+    if (botan_nist_kw_dec(
+          name, 0, in->m, in->mlen, kek.data(), kek_len, deckey.data(), &deckey_len)) {
+#else
     if (botan_key_unwrap3394(
           in->m, in->mlen, kek.data(), kek_len, deckey.data(), &deckey_len)) {
+#endif
         goto end;
     }
 
