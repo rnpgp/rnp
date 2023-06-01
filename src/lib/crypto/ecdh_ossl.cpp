c361a50e607b937ac9b70a86a741f83e2d92129c
Nickolay Olshevsky <o.nickolay@gmail.com>
2023-06-01 12:57:32 +0300

    OpenSSL 3.0: Do not use deprecated functions for EC operations.

--- src/lib/crypto/ecdh_ossl.cpp
+++ src/lib/crypto/ecdh_ossl.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022, [Ribose Inc](https://www.ribose.com).
+ * Copyright (c) 2021-2023, [Ribose Inc](https://www.ribose.com).
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
