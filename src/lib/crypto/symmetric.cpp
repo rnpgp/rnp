63a0a162500daa4f70096977ec6a8f83c45dfe97
Denys <desvxx@proton.me>
2024-04-28 15:52:46 +0300

    Move duplicated parts from symmetric*.cpp to separate file

--- src/lib/crypto/symmetric.cpp
+++ src/lib/crypto/symmetric.cpp
@@ -195,14 +195,6 @@ pgp_cipher_cfb_start(pgp_crypt_t *  crypt,
     return true;
 }
 
-void
-pgp_cipher_cfb_resync(pgp_crypt_t *crypt, const uint8_t *buf)
-{
-    /* iv will be encrypted in the upcoming call to encrypt/decrypt */
-    memcpy(crypt->cfb.iv, buf, crypt->blocksize);
-    crypt->cfb.remaining = 0;
-}
-
 int
 pgp_cipher_cfb_finish(pgp_crypt_t *crypt)
 {
@@ -383,67 +375,6 @@ pgp_cipher_cfb_decrypt(pgp_crypt_t *crypt, uint8_t *out, const uint8_t *in, size
     return 0;
 }
 
-size_t
-pgp_cipher_block_size(pgp_crypt_t *crypt)
-{
-    return crypt->blocksize;
-}
-
-unsigned
-pgp_block_size(pgp_symm_alg_t alg)
-{
-    switch (alg) {
-    case PGP_SA_IDEA:
-    case PGP_SA_TRIPLEDES:
-    case PGP_SA_CAST5:
-    case PGP_SA_BLOWFISH:
-        return 8;
-    case PGP_SA_AES_128:
-    case PGP_SA_AES_192:
-    case PGP_SA_AES_256:
-    case PGP_SA_TWOFISH:
-    case PGP_SA_CAMELLIA_128:
-    case PGP_SA_CAMELLIA_192:
-    case PGP_SA_CAMELLIA_256:
-    case PGP_SA_SM4:
-        return 16;
-    default:
-        return 0;
-    }
-}
-
-unsigned
-pgp_key_size(pgp_symm_alg_t alg)
-{
-    /* Update MAX_SYMM_KEY_SIZE after adding algorithm
-     * with bigger key size.
-     */
-    static_assert(32 == MAX_SYMM_KEY_SIZE, "MAX_SYMM_KEY_SIZE must be updated");
-
-    switch (alg) {
-    case PGP_SA_IDEA:
-    case PGP_SA_CAST5:
-    case PGP_SA_BLOWFISH:
-    case PGP_SA_AES_128:
-    case PGP_SA_CAMELLIA_128:
-    case PGP_SA_SM4:
-        return 16;
-
-    case PGP_SA_TRIPLEDES:
-    case PGP_SA_AES_192:
-    case PGP_SA_CAMELLIA_192:
-        return 24;
-
-    case PGP_SA_TWOFISH:
-    case PGP_SA_AES_256:
-    case PGP_SA_CAMELLIA_256:
-        return 32;
-
-    default:
-        return 0;
-    }
-}
-
 bool
 pgp_is_sa_supported(int alg, bool silent)
 {
@@ -493,39 +424,6 @@ pgp_cipher_aead_init(pgp_crypt_t *  crypt,
     return true;
 }
 
-size_t
-pgp_cipher_aead_granularity(pgp_crypt_t *crypt)
-{
-    return crypt->aead.granularity;
-}
-#endif
-
-size_t
-pgp_cipher_aead_nonce_len(pgp_aead_alg_t aalg)
-{
-    switch (aalg) {
-    case PGP_AEAD_EAX:
-        return PGP_AEAD_EAX_NONCE_LEN;
-    case PGP_AEAD_OCB:
-        return PGP_AEAD_OCB_NONCE_LEN;
-    default:
-        return 0;
-    }
-}
-
-size_t
-pgp_cipher_aead_tag_len(pgp_aead_alg_t aalg)
-{
-    switch (aalg) {
-    case PGP_AEAD_EAX:
-    case PGP_AEAD_OCB:
-        return PGP_AEAD_EAX_OCB_TAG_LEN;
-    default:
-        return 0;
-    }
-}
-
-#if defined(ENABLE_AEAD)
 bool
 pgp_cipher_aead_set_ad(pgp_crypt_t *crypt, const uint8_t *ad, size_t len)
 {
@@ -620,33 +518,5 @@ pgp_cipher_aead_destroy(pgp_crypt_t *crypt)
     memset(crypt, 0x0, sizeof(*crypt));
 }
 
-size_t
-pgp_cipher_aead_nonce(pgp_aead_alg_t aalg, const uint8_t *iv, uint8_t *nonce, size_t index)
-{
-    switch (aalg) {
-    case PGP_AEAD_EAX:
-        /* The nonce for EAX mode is computed by treating the starting
-        initialization vector as a 16-octet, big-endian value and
-        exclusive-oring the low eight octets of it with the chunk index.
-        */
-        memcpy(nonce, iv, PGP_AEAD_EAX_NONCE_LEN);
-        for (int i = 15; (i > 7) && index; i--) {
-            nonce[i] ^= index & 0xff;
-            index = index >> 8;
-        }
-        return PGP_AEAD_EAX_NONCE_LEN;
-    case PGP_AEAD_OCB:
-        /* The nonce for a chunk of chunk index "i" in OCB processing is defined as:
-           OCB-Nonce_{i} = IV[1..120] xor i
-        */
-        memcpy(nonce, iv, PGP_AEAD_OCB_NONCE_LEN);
-        for (int i = 14; (i >= 0) && index; i--) {
-            nonce[i] ^= index & 0xff;
-            index = index >> 8;
-        }
-        return PGP_AEAD_OCB_NONCE_LEN;
-    default:
-        return 0;
-    }
-}
+
 #endif // ENABLE_AEAD
