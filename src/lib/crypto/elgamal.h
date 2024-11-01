7d0216016b22ff5265a23128fe3e4c959df939b9
Nickolay Olshevsky <o.nickolay@gmail.com>
2024-11-01 18:01:27 +0200

    Refactor ElGamal code to be closer to C++ style.

--- src/lib/crypto/elgamal.h
+++ src/lib/crypto/elgamal.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2017-2022 Ribose Inc.
+ * Copyright (c) 2017-2024 Ribose Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,7 +31,25 @@
 #include "crypto/rng.h"
 #include "crypto/mpi.h"
 
-typedef struct pgp_eg_key_t {
+namespace pgp {
+namespace eg {
+
+class Signature {
+  public:
+    /* This is kept only for packet reading. Implementation MUST
+     * not create elgamal signatures */
+    pgp::mpi r;
+    pgp::mpi s;
+};
+
+class Encrypted {
+  public:
+    pgp::mpi g;
+    pgp::mpi m;
+};
+
+class Key {
+  public:
     pgp::mpi p;
     pgp::mpi g;
     pgp::mpi y;
@@ -44,84 +62,74 @@ typedef struct pgp_eg_key_t {
         x.forget();
     }
 
-    ~pgp_eg_key_t()
+    ~Key()
     {
         clear_secret();
     }
-} pgp_eg_key_t;
 
-typedef struct pgp_eg_signature_t {
-    /* This is kept only for packet reading. Implementation MUST
-     * not create elgamal signatures */
-    pgp::mpi r;
-    pgp::mpi s;
-} pgp_eg_signature_t;
+    bool validate(bool secret) const noexcept;
 
-typedef struct pgp_eg_encrypted_t {
-    pgp::mpi g;
-    pgp::mpi m;
-} pgp_eg_encrypted_t;
+    /*
+     * Performs ElGamal encryption
+     * Result of an encryption is composed of two parts - g2k and encm
+     *
+     * @param rng initialized rnp::RNG
+     * @param out encryption result
+     * @param in plaintext to be encrypted
+     * @param in_len length of the plaintext
+     * @param key public key to be used for encryption
+     *
+     * @pre out: must be valid pointer to corresponding structure
+     * @pre in_len: can't be bigger than byte size of `p'
+     *
+     * @return RNP_SUCCESS
+     *         RNP_ERROR_OUT_OF_MEMORY  allocation failure
+     *         RNP_ERROR_BAD_PARAMETERS wrong input provided
+     */
+    rnp_result_t encrypt_pkcs1(rnp::RNG &     rng,
+                               Encrypted &    out,
+                               const uint8_t *in,
+                               size_t         in_len) const;
 
-bool elgamal_validate_key(const pgp_eg_key_t *key, bool secret);
+    /*
+     * Performs ElGamal decryption
+     *
+     * @param rng initialized rnp::RNG
+     * @param out decrypted plaintext. Must be capable of storing at least as much bytes as p
+     * size
+     * @param out_len number of plaintext bytes written will be put here
+     * @param in encrypted data
+     * @param key private key
+     *
+     * @pre out, in: must be valid pointers
+     * @pre out: length must be long enough to store decrypted data. Max size of
+     *           decrypted data is equal to bytes size of `p'
+     *
+     * @return RNP_SUCCESS
+     *         RNP_ERROR_OUT_OF_MEMORY  allocation failure
+     *         RNP_ERROR_BAD_PARAMETERS wrong input provided
+     */
+    rnp_result_t decrypt_pkcs1(rnp::RNG &       rng,
+                               uint8_t *        out,
+                               size_t *         out_len,
+                               const Encrypted &in) const;
 
-/*
- * Performs ElGamal encryption
- * Result of an encryption is composed of two parts - g2k and encm
- *
- * @param rng initialized rnp::RNG
- * @param out encryption result
- * @param in plaintext to be encrypted
- * @param in_len length of the plaintext
- * @param key public key to be used for encryption
- *
- * @pre out: must be valid pointer to corresponding structure
- * @pre in_len: can't be bigger than byte size of `p'
- *
- * @return RNP_SUCCESS
- *         RNP_ERROR_OUT_OF_MEMORY  allocation failure
- *         RNP_ERROR_BAD_PARAMETERS wrong input provided
- */
-rnp_result_t elgamal_encrypt_pkcs1(rnp::RNG *          rng,
-                                   pgp_eg_encrypted_t *out,
-                                   const uint8_t *     in,
-                                   size_t              in_len,
-                                   const pgp_eg_key_t *key);
+    /*
+     * Generates ElGamal key
+     *
+     * @param rng pointer to PRNG
+     * @param key generated key
+     * @param keybits key bitlen
+     *
+     * @pre `keybits' > 1024
+     *
+     * @returns RNP_ERROR_BAD_PARAMETERS wrong parameters provided
+     *          RNP_ERROR_GENERIC internal error
+     *          RNP_SUCCESS key generated and copied to `seckey'
+     */
+    rnp_result_t generate(rnp::RNG &rng, size_t keybits);
+};
 
-/*
- * Performs ElGamal decryption
- *
- * @param rng initialized rnp::RNG
- * @param out decrypted plaintext. Must be capable of storing at least as much bytes as p size
- * @param out_len number of plaintext bytes written will be put here
- * @param in encrypted data
- * @param key private key
- *
- * @pre out, in: must be valid pointers
- * @pre out: length must be long enough to store decrypted data. Max size of
- *           decrypted data is equal to bytes size of `p'
- *
- * @return RNP_SUCCESS
- *         RNP_ERROR_OUT_OF_MEMORY  allocation failure
- *         RNP_ERROR_BAD_PARAMETERS wrong input provided
- */
-rnp_result_t elgamal_decrypt_pkcs1(rnp::RNG *                rng,
-                                   uint8_t *                 out,
-                                   size_t *                  out_len,
-                                   const pgp_eg_encrypted_t *in,
-                                   const pgp_eg_key_t *      key);
-
-/*
- * Generates ElGamal key
- *
- * @param rng pointer to PRNG
- * @param key generated key
- * @param keybits key bitlen
- *
- * @pre `keybits' > 1024
- *
- * @returns RNP_ERROR_BAD_PARAMETERS wrong parameters provided
- *          RNP_ERROR_GENERIC internal error
- *          RNP_SUCCESS key generated and copied to `seckey'
- */
-rnp_result_t elgamal_generate(rnp::RNG *rng, pgp_eg_key_t *key, size_t keybits);
+} // namespace eg
+} // namespace pgp
 #endif
