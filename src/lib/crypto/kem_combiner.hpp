22b82ed3510498e002c55353d5a1708380ffa6ec
Johannes Roth <johannes.roth@mtg.de>
2025-04-10 17:48:57 +0200

    adjust kem combiner to include the length byte

--- src/lib/crypto/kem_combiner.hpp
+++ src/lib/crypto/kem_combiner.hpp
@@ -40,13 +40,16 @@ class PqcKemCombiner {
     /* The value domSep is a constant set to the UTF-8 encoding of the
        string "OpenPGPCompositeKDFv1", i.e.
        domSep := 4F 70 65 6E 50 47 50 43 6F 6D 70 6F 73 69 74 65 4B 44 46 76 31
+       len(domSep) = 0x15
+
+       We also concatenate len(domSep) here
     */
     static std::vector<uint8_t>
     domSeparation()
     {
         return std::vector<uint8_t>({0x4F, 0x70, 0x65, 0x6E, 0x50, 0x47, 0x50,
                                      0x43, 0x6F, 0x6D, 0x70, 0x6F, 0x73, 0x69,
-                                     0x74, 0x65, 0x4B, 0x44, 0x46, 0x76, 0x31});
+                                     0x74, 0x65, 0x4B, 0x44, 0x46, 0x76, 0x31, 0x15});
     }
 
   public:
