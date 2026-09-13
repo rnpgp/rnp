a2c47c991ed4caf42f139ea83859a1e87bfa96d5
MakerYuichi <106516578+MakerYuichi@users.noreply.github.com>
2026-09-13 19:56:52 +0530

    fix(rnp): skip default filename/mtime embedding in crypto-refresh builds

--- src/rnp/fficli.cpp
+++ src/rnp/fficli.cpp
@@ -2771,11 +2771,13 @@ cli_rnp_sign(const rnp_cfg &cfg, cli_rnp_t *rnp, rnp_input_t input, rnp_output_t
                 goto done;
             }
         } else if (cfg.has(CFG_INFILE)) {
+#if !defined(ENABLE_CRYPTO_REFRESH)
             const std::string &fname = cfg.get_str(CFG_INFILE);
             if (rnp_op_sign_set_file_name(op, extract_filename(fname).c_str())) {
                 goto done;
             }
             rnp_op_sign_set_file_mtime(op, rnp_filemtime(fname.c_str()));
+#endif
         }
         if (rnp_op_sign_set_compression(op, cfg.get_cstr(CFG_ZALG), cfg.get_int(CFG_ZLEVEL))) {
             goto done;
@@ -2844,11 +2846,13 @@ cli_rnp_encrypt_and_sign(const rnp_cfg &cfg,
             goto done;
         }
     } else if (cfg.has(CFG_INFILE)) {
+#if !defined(ENABLE_CRYPTO_REFRESH)
         const std::string &fname = cfg.get_str(CFG_INFILE);
         if (rnp_op_encrypt_set_file_name(op, extract_filename(fname).c_str())) {
             goto done;
         }
         rnp_op_encrypt_set_file_mtime(op, rnp_filemtime(fname.c_str()));
+#endif
     }
 
     if (rnp_op_encrypt_set_compression(op, cfg.get_cstr(CFG_ZALG), cfg.get_int(CFG_ZLEVEL))) {
